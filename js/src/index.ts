0a5ea6007adf2f55360f5ec5e554eb4d456161d0
Zander Chase <130414180+vowelparrot@users.noreply.github.com>
2023-06-05 10:53:49 -0700

    Use Evaluation index file rather than exposing sub-files (#18)

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,5 +1,12 @@
 export { LangChainPlusClient } from "./client.js";
 
-export { Dataset, Example, TracerSession, Run, RunResult } from "./schemas.js";
+export {
+  Dataset,
+  Example,
+  TracerSession,
+  Run,
+  RunResult,
+  Feedback,
+} from "./schemas.js";
 
 export { RunTree } from "./run_trees.js";
