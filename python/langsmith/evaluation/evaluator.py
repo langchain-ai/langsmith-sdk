819bb76049fa57095e0b1fb49ef6709d2ae03677
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-09-08 16:38:31 -0700

    lint

--- python/langsmith/evaluation/evaluator.py
+++ python/langsmith/evaluation/evaluator.py
@@ -1,7 +1,7 @@
 import asyncio
+import uuid
 from abc import abstractmethod
-from typing import Dict, Optional
-from langsmith.client import ID_TYPE
+from typing import Dict, Optional, Union
 
 try:
     from pydantic.v1 import BaseModel, Field  # type: ignore[import]
@@ -26,7 +26,7 @@ class EvaluationResult(BaseModel):
     """What the correct value should be, if applicable."""
     evaluator_info: Dict = Field(default_factory=dict)
     """Additional information about the evaluator."""
-    source_run_id: Optional[ID_TYPE] = None
+    source_run_id: Optional[Union[uuid.UUID, str]] = None
 
     class Config:
         """Pydantic model configuration."""
