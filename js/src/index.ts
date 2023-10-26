663f4ab73ff51beff6f5de68480a5344bf89aac3
Chase McDougall <chasemcdougall@hotmail.com>
2023-10-26 10:13:51 -0400

    Add RunTreeConfig to index imports

--- js/src/index.ts
+++ js/src/index.ts
@@ -2,4 +2,4 @@ export { Client } from "./client.js";
 
 export { Dataset, Example, TracerSession, Run, Feedback } from "./schemas.js";
 
-export { RunTree } from "./run_trees.js";
+export { RunTree, RunTreeConfig } from "./run_trees.js";
