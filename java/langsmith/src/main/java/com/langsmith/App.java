e583c2b6e18be1f3ecef88e1e7802217cc6f9e63
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2024-02-07 00:38:51 -0800

    tmp

---
+++ java/langsmith/src/main/java/com/langsmith/App.java
@@ -0,0 +1,13 @@
+package com.langsmith;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
