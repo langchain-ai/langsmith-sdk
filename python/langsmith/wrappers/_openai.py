4804e4c5badb652d756f4570a7700bb08b938663
Nuno Campos <nuno@langchain.dev>
2024-02-10 13:18:24 -0800

    OpenAI Wrapper: Don't ignore function/tool calls in openai streaming output

--- python/langsmith/wrappers/_openai.py
+++ python/langsmith/wrappers/_openai.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 import functools
-from typing import TYPE_CHECKING, Callable, List, TypeVar, Union
+from collections import defaultdict
+from typing import TYPE_CHECKING, Any, Callable, DefaultDict, Dict, List, TypeVar, Union
 
 from langsmith import run_helpers
 
@@ -9,25 +10,83 @@ if TYPE_CHECKING:
     from openai import AsyncOpenAI, OpenAI
     from openai.types.chat.chat_completion_chunk import (
         ChatCompletionChunk,
+        Choice,
+        ChoiceDeltaToolCall,
     )
     from openai.types.completion import Completion
 
 C = TypeVar("C", bound=Union["OpenAI", "AsyncOpenAI"])
 
 
+def _reduce_choices(choices: List[Choice]) -> dict:
+    reversed_choices = list(reversed(choices))
+    message: Dict[str, Any] = {
+        "role": "assistant",
+        "content": "",
+    }
+    for c in reversed_choices:
+        if c.delta.role:
+            message["role"] = c.delta.role
+            break
+    tool_calls: DefaultDict[int, List[ChoiceDeltaToolCall]] = defaultdict(list)
+    for c in choices:
+        if c.delta.content:
+            message["content"] += c.delta.content
+        if c.delta.function_call:
+            if not message.get("function_call"):
+                message["function_call"] = {"name": "", "arguments": ""}
+            if c.delta.function_call.name:
+                message["function_call"]["name"] += c.delta.function_call.name
+            if c.delta.function_call.arguments:
+                message["function_call"]["arguments"] += c.delta.function_call.arguments
+        if c.delta.tool_calls:
+            for tool_call in c.delta.tool_calls:
+                tool_calls[c.index].append(tool_call)
+    if tool_calls:
+        message["tool_calls"] = [None for _ in tool_calls.keys()]
+        for index, tool_call_chunks in tool_calls.items():
+            message["tool_calls"][index] = {
+                "index": index,
+                "id": next((c.id for c in tool_call_chunks if c.id), None),
+                "type": next((c.type for c in tool_call_chunks if c.type), None),
+            }
+            for chunk in tool_call_chunks:
+                if chunk.function:
+                    if not message["tool_calls"][index].get("function"):
+                        message["tool_calls"][index]["function"] = {
+                            "name": "",
+                            "arguments": "",
+                        }
+                    if chunk.function.name:
+                        message["tool_calls"][index]["function"][
+                            "name"
+                        ] += chunk.function.name
+                    if chunk.function.arguments:
+                        message["tool_calls"][index]["function"][
+                            "arguments"
+                        ] += chunk.function.arguments
+    return {
+        "index": choices[0].index,
+        "finish_reason": next(
+            (c.finish_reason for c in reversed_choices if c.finish_reason),
+            None,
+        ),
+        "message": message,
+    }
+
+
 def _reduce_chat(all_chunks: List[ChatCompletionChunk]) -> dict:
-    all_content = []
+    choices_by_index: DefaultDict[int, List[Choice]] = defaultdict(list)
     for chunk in all_chunks:
-        content = chunk.choices[0].delta.content
-        if content is not None:
-            all_content.append(content)
-    content = "".join(all_content)
+        for choice in chunk.choices:
+            choices_by_index[choice.index].append(choice)
     if all_chunks:
         d = all_chunks[-1].model_dump()
-        d["choices"] = [{"message": {"role": "assistant", "content": content}}]
+        d["choices"] = [
+            _reduce_choices(choices) for choices in choices_by_index.values()
+        ]
     else:
-        d = {"choices": [{"message": {"role": "assistant", "content": content}}]}
-
+        d = {"choices": [{"message": {"role": "assistant", "content": ""}}]}
     return d
 
 
