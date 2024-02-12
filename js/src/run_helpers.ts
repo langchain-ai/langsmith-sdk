dec34dfee386e8cedb8fc840437c01a6404cf457
Tat Dat Duong <david@duong.cz>
2024-02-12 01:39:46 +0100

    Add traceable run helper

---
+++ js/src/run_helpers.ts
@@ -0,0 +1,69 @@
+import { RunTree, RunTreeConfig } from "./run_trees.js";
+import { KVMap } from "./schemas.js";
+
+export const traceable = (params: RunTreeConfig) => {
+  return <I, O>(func: (rawInput: I, parentRun: RunTree | null) => O) => {
+    async function wrappedFunc(
+      rawInput: I,
+      parentRun: RunTree | { root: RunTree } | null
+    ) {
+      if (parentRun == null) {
+        return await func(rawInput, parentRun);
+      }
+
+      const inputs: KVMap =
+        typeof rawInput === "object" && rawInput != null
+          ? rawInput
+          : { input: rawInput };
+
+      const currentRun: RunTree =
+        "root" in parentRun
+          ? parentRun.root
+          : await parentRun.createChild({
+              ...params,
+              inputs,
+            });
+
+      if ("root" in parentRun) {
+        Object.assign(currentRun, { ...params, inputs });
+      }
+
+      const initialOutputs = currentRun.outputs;
+      const initialError = currentRun.error;
+
+      try {
+        const rawOutput = await func(rawInput, currentRun);
+        const outputs: KVMap =
+          typeof rawOutput === "object" &&
+          rawOutput != null &&
+          !Array.isArray(rawOutput)
+            ? rawOutput
+            : { outputs: rawOutput };
+
+        if (initialOutputs === currentRun.outputs) {
+          await currentRun.end(outputs);
+        } else {
+          currentRun.end_time = Date.now();
+        }
+
+        return rawOutput;
+      } catch (error) {
+        if (initialError === currentRun.error) {
+          await currentRun.end(initialOutputs, String(error));
+        } else {
+          currentRun.end_time = Date.now();
+        }
+
+        throw error;
+      } finally {
+        await currentRun.postRun();
+      }
+    }
+
+    Object.defineProperty(wrappedFunc, "langsmith:traceable", {
+      value: params,
+    });
+
+    return wrappedFunc;
+  };
+};
