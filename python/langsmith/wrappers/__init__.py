9a3fe06a6fc515af4c1f18e836e580bdf628a909
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2024-03-05 19:44:40 -0800

    Add generic example

--- python/langsmith/wrappers/__init__.py
+++ python/langsmith/wrappers/__init__.py
@@ -1,5 +1,6 @@
 """This module provides convenient tracing wrappers for popular libraries."""
 
+from langsmith.wrappers._generic import wrap_sdk
 from langsmith.wrappers._openai import wrap_openai
 
-__all__ = ["wrap_openai"]
+__all__ = ["wrap_openai", "wrap_sdk"]
