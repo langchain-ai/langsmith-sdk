eb1d36d8e280f2ae0fc61838041b54a576746235
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2024-03-05 16:20:15 -0800

    Update

--- python/langsmith/client.py
+++ python/langsmith/client.py
@@ -2202,6 +2202,76 @@ class Client:
             _tenant_id=self._get_optional_tenant_id(),
         )
 
+    def diff_dataset_versions(
+        self,
+        dataset_id: Optional[ID_TYPE] = None,
+        *,
+        dataset_name: Optional[str] = None,
+        from_version: Union[str, datetime.datetime],
+        to_version: Union[str, datetime.datetime],
+    ) -> ls_schemas.DatasetDiffInfo:
+        """Get the difference between two versions of a dataset.
+
+        Parameters
+        ----------
+        dataset_id : str or None, default=None
+            The ID of the dataset.
+        dataset_name : str or None, default=None
+            The name of the dataset.
+        from_version : str or datetime.datetime
+            The starting version for the diff.
+        to_version : str or datetime.datetime
+            The ending version for the diff.
+
+        Returns:
+        -------
+        DatasetDiffInfo
+            The difference between the two versions of the dataset.
+
+        Examples:
+        .. code-block:: python
+
+            # Get the difference between two tagged versions of a dataset
+            from_version = "prod"
+            to_version = "dev"
+            diff = client.diff_dataset_versions(
+                dataset_name="my-dataset",
+                from_version=from_version,
+                to_version=to_version,
+            )
+            print(diff)
+
+            # Get the difference between two timestamped versions of a dataset
+
+            from_version = datetime.datetime(2024, 1, 1)
+            to_version = datetime.datetime(2024, 2, 1)
+            diff = client.diff_dataset_versions(
+                dataset_name="my-dataset",
+                from_version=from_version,
+                to_version=to_version,
+            )
+            print(diff)
+        """
+        if dataset_id is None:
+            if dataset_name is None:
+                raise ValueError("Must provide either dataset name or ID")
+            dataset_id = self.read_dataset(dataset_name=dataset_name).id
+        dsid = _as_uuid(dataset_id, "dataset_id")
+        response = self.session.get(
+            f"{self.api_url}/datasets/{dsid}/versions/diff",
+            headers=self._headers,
+            params={
+                "from": from_version.isoformat()
+                if isinstance(from_version, datetime.datetime)
+                else from_version,
+                "to": to_version.isoformat()
+                if isinstance(to_version, datetime.datetime)
+                else to_version,
+            },
+        )
+        ls_utils.raise_for_status_with_text(response)
+        return ls_schemas.DatasetDiffInfo(**response.json())
+
     def read_dataset_openai_finetuning(
         self, dataset_id: Optional[str] = None, *, dataset_name: Optional[str] = None
     ) -> list:
