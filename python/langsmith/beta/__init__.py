5062959c7796a18695a20acd5e05afa7fe8f807d
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2024-03-19 15:00:43 -0700

    Add beta

--- python/langsmith/beta/__init__.py
+++ python/langsmith/beta/__init__.py
@@ -1,5 +1,5 @@
 """Beta functionality prone to change."""
 
-from langsmith.beta._evals import convert_runs_to_test
+from langsmith.beta._evals import compute_test_metrics, convert_runs_to_test
 
-__all__ = ["convert_runs_to_test"]
+__all__ = ["convert_runs_to_test", "compute_test_metrics"]
