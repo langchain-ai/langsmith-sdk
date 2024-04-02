7fccbb2226b1b3c9622f3383435f36091d7b1157
Erick Friis <erick@langchain.dev>
2024-04-02 15:39:19 -0700

    getattr for reduced cold start times

--- python/langsmith/__init__.py
+++ python/langsmith/__init__.py
@@ -1,17 +1,43 @@
 """LangSmith Client."""
 
-from importlib import metadata
+from typing import Any
 
-try:
-    __version__ = metadata.version(__package__)
-except metadata.PackageNotFoundError:
-    # Case where package metadata is not available.
-    __version__ = ""
 
-from langsmith.client import Client
-from langsmith.evaluation.evaluator import EvaluationResult, RunEvaluator
-from langsmith.run_helpers import trace, traceable
-from langsmith.run_trees import RunTree
+def __getattr__(name: str) -> Any:
+    if name == "__version__":
+        try:
+            from importlib import metadata
+
+            return metadata.version(__package__)
+        except metadata.PackageNotFoundError:
+            return ""
+    elif name == "Client":
+        from langsmith.client import Client
+
+        return Client
+    elif name == "RunTree":
+        from langsmith.run_trees import RunTree
+
+        return RunTree
+    elif name == "EvaluationResult":
+        from langsmith.evaluation.evaluator import EvaluationResult
+
+        return EvaluationResult
+    elif name == "RunEvaluator":
+        from langsmith.evaluation.evaluator import RunEvaluator
+
+        return RunEvaluator
+    elif name == "trace":
+        from langsmith.run_helpers import trace
+
+        return trace
+    elif name == "traceable":
+        from langsmith.run_helpers import traceable
+
+        return traceable
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
 
 __all__ = [
     "Client",
