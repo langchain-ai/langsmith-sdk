1025a941cab2eb80540e6aac6a3487beef616f01
bracesproul <braceasproul@gmail.com>
2024-04-29 09:30:11 -0700

    chore: lint files

--- js/src/utils/_git.ts
+++ js/src/utils/_git.ts
@@ -1,5 +1,3 @@
-import { exec, execSync } from "child_process";
-
 interface GitInfo {
   remoteUrl?: string | null;
   commit?: string | null;
@@ -12,9 +10,17 @@ interface GitInfo {
   tags?: string | null;
 }
 
-const execGit = (command: string[]): Promise<string | null> => {
+async function importChildProcess() {
+  const { exec } = await import("child_process");
+  return { exec };
+}
+
+const execGit = (
+  command: string[],
+  exec: (...args: any[]) => any
+): Promise<string | null> => {
   return new Promise((resolve) => {
-    exec(`git ${command.join(" ")}`, (error, stdout) => {
+    exec(`git ${command.join(" ")}`, (error: any, stdout: any) => {
       if (error) {
         resolve(null);
       } else {
@@ -24,54 +30,15 @@ const execGit = (command: string[]): Promise<string | null> => {
   });
 };
 
-const execSyncGit = (command: string[]): string | null => {
-  try {
-    return execSync(`git ${command.join(" ")}`, { encoding: "utf8" }).trim();
-  } catch (error) {
-    return null;
-  }
-};
-
-export const getGitInfoSync = (remote = "origin"): GitInfo | null => {
-  const isInsideWorkTree = execSyncGit(["rev-parse", "--is-inside-work-tree"]);
-  if (!isInsideWorkTree) {
-    return null;
-  }
-
-  const remoteUrl = execSyncGit(["remote", "get-url", remote]);
-  const commit = execSyncGit(["rev-parse", "HEAD"]);
-  const commitTime = execSyncGit(["log", "-1", "--format=%ct"]);
-  const branch = execSyncGit(["rev-parse", "--abbrev-ref", "HEAD"]);
-  const tags = execSyncGit([
-    "describe",
-    "--tags",
-    "--exact-match",
-    "--always",
-    "--dirty",
-  ]);
-  const dirty = execSyncGit(["status", "--porcelain"]) !== "";
-  const authorName = execSyncGit(["log", "-1", "--format=%an"]);
-  const authorEmail = execSyncGit(["log", "-1", "--format=%ae"]);
-
-  return {
-    remoteUrl,
-    commit,
-    commitTime,
-    branch,
-    tags,
-    dirty,
-    authorName,
-    authorEmail,
-  };
-};
-
 export const getGitInfo = async (
   remote = "origin"
 ): Promise<GitInfo | null> => {
-  const isInsideWorkTree = await execGit([
-    "rev-parse",
-    "--is-inside-work-tree",
-  ]);
+  const { exec } = await importChildProcess();
+
+  const isInsideWorkTree = await execGit(
+    ["rev-parse", "--is-inside-work-tree"],
+    exec
+  );
   if (!isInsideWorkTree) {
     return null;
   }
@@ -86,14 +53,17 @@ export const getGitInfo = async (
     authorName,
     authorEmail,
   ] = await Promise.all([
-    execGit(["remote", "get-url", remote]),
-    execGit(["rev-parse", "HEAD"]),
-    execGit(["log", "-1", "--format=%ct"]),
-    execGit(["rev-parse", "--abbrev-ref", "HEAD"]),
-    execGit(["describe", "--tags", "--exact-match", "--always", "--dirty"]),
-    execGit(["status", "--porcelain"]).then((output) => output !== ""),
-    execGit(["log", "-1", "--format=%an"]),
-    execGit(["log", "-1", "--format=%ae"]),
+    execGit(["remote", "get-url", remote], exec),
+    execGit(["rev-parse", "HEAD"], exec),
+    execGit(["log", "-1", "--format=%ct"], exec),
+    execGit(["rev-parse", "--abbrev-ref", "HEAD"], exec),
+    execGit(
+      ["describe", "--tags", "--exact-match", "--always", "--dirty"],
+      exec
+    ),
+    execGit(["status", "--porcelain"], exec).then((output) => output !== ""),
+    execGit(["log", "-1", "--format=%an"], exec),
+    execGit(["log", "-1", "--format=%ae"], exec),
   ]);
 
   return {
