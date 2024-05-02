b413ea321f185db33942350e0c57fff20dd5198a
Tat Dat Duong <david@duong.cz>
2024-05-02 21:54:01 +0200

    Update unit tests

--- js/src/tests/traceable.test.ts
+++ js/src/tests/traceable.test.ts
@@ -1,75 +1,35 @@
-import { jest } from "@jest/globals";
 import type { ROOT as TRoot } from "../traceable.js";
 import type { RunTree } from "../run_trees.js";
-
-const mockCounter = jest.fn();
-
-const treeState: {
-  nodes: string[];
-  edges: [string, string][];
-} = { nodes: [], edges: [] };
-
-const mockEnd = jest.fn();
-const mockTree = jest.fn().mockImplementation((...args) => {
-  mockCounter();
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  const name = args.at(0)?.name as string | undefined;
-  const id = name + ":" + mockCounter.mock.calls.length;
-  treeState.nodes.push(id);
-
-  const lastArg = args.at(-1);
-  if (typeof lastArg === "string") {
-    treeState.edges.push([lastArg, id]);
-  }
-
-  return {
-    createChild: (...args: unknown[]) => mockTree(...args, id),
-    end: (...args: unknown[]) => mockEnd(...args, id),
-    postRun: jest.fn(),
-    patchRun: jest.fn(),
-  };
-});
-
-jest.unstable_mockModule("../run_trees.js", () => {
-  return {
-    RunTree: mockTree,
-    isRunTree: (x: unknown) =>
-      x !== undefined &&
-      typeof (x as RunTree).createChild === "function" &&
-      typeof (x as RunTree).postRun === "function",
-    isRunnableConfigLike: jest.fn().mockReturnValue(false),
-  };
-});
-
-beforeEach(() => {
-  jest.clearAllMocks();
-  treeState.nodes = [];
-  treeState.edges = [];
-});
+import { getAssumedTreeFromCalls } from "./utils/tree.js";
+import { mockClient } from "./utils/mock_client.js";
 
 test("basic traceable implementation", async () => {
+  const { client, callSpy } = mockClient();
   const { traceable } = await import("../traceable.js");
 
-  const llm = traceable(async function* llm(input: string) {
-    const response = input.repeat(2).split("");
-    for (const char of response) {
-      yield char;
-    }
-  });
+  const llm = traceable(
+    async function* llm(input: string) {
+      const response = input.repeat(2).split("");
+      for (const char of response) {
+        yield char;
+      }
+    },
+    { client }
+  );
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   for await (const _ of llm("Hello world")) {
     // pass
   }
 
-  mockCounter.mock;
-
-  expect(mockTree).toBeCalledTimes(1);
-  expect(mockTree).toBeCalledWith({ name: "llm", tags: [], metadata: {} });
+  expect(getAssumedTreeFromCalls(callSpy.mock.calls)).toEqual({
+    nodes: ["llm:0"],
+    edges: [],
+  });
 });
 
 test("nested traceable implementation", async () => {
+  const { client, callSpy } = mockClient();
   const { traceable } = await import("../traceable.js");
 
   const llm = traceable(async function llm(input: string) {
@@ -83,16 +43,19 @@ test("nested traceable implementation", async () => {
     }
   });
 
-  const chain = traceable(async function chain(input: string) {
-    const question = await llm(input);
+  const chain = traceable(
+    async function chain(input: string) {
+      const question = await llm(input);
 
-    let answer = "";
-    for await (const char of str(question)) {
-      answer += char;
-    }
+      let answer = "";
+      for await (const char of str(question)) {
+        answer += char;
+      }
 
-    return { question, answer };
-  });
+      return { question, answer };
+    },
+    { client }
+  );
 
   const result = await chain("Hello world");
 
@@ -101,19 +64,17 @@ test("nested traceable implementation", async () => {
     answer: "dlrow olleHdlrow olleH",
   });
 
-  expect(mockTree).toBeCalledTimes(3);
-  expect(mockTree).toBeCalledWith({ name: "chain", tags: [], metadata: {} });
-  expect(mockTree).toBeCalledWith(
-    { name: "llm", tags: [], metadata: {} },
-    "chain:1"
-  );
-  expect(mockTree).toBeCalledWith(
-    { name: "str", tags: [], metadata: {} },
-    "chain:1"
-  );
+  expect(getAssumedTreeFromCalls(callSpy.mock.calls)).toEqual({
+    nodes: ["chain:0", "llm:1", "str:2"],
+    edges: [
+      ["chain:0", "llm:1"],
+      ["chain:0", "str:2"],
+    ],
+  });
 });
 
 test("passing run tree manually", async () => {
+  const { client, callSpy } = mockClient();
   const { traceable, ROOT } = await import("../traceable.js");
 
   const child = traceable(
@@ -126,21 +87,35 @@ test("passing run tree manually", async () => {
     { name: "child" }
   );
 
-  const parent = traceable(async function parent(runTree: RunTree) {
-    const first = await child(runTree);
-    const second = await child(runTree);
+  const parent = traceable(
+    async function parent(runTree: RunTree) {
+      const first = await child(runTree);
+      const second = await child(runTree);
 
-    return first + second;
-  });
+      return first + second;
+    },
+    { client }
+  );
 
   await parent(ROOT as typeof TRoot);
-  expect(mockTree).toBeCalledTimes(7);
-  expect(treeState.edges).toEqual([
-    ["parent:1", "child:2"],
-    ["child:2", "child:3"],
-    ["child:3", "child:4"],
-    ["parent:1", "child:5"],
-    ["child:5", "child:6"],
-    ["child:6", "child:7"],
-  ]);
+
+  expect(getAssumedTreeFromCalls(callSpy.mock.calls)).toEqual({
+    nodes: [
+      "parent:0",
+      "child:1",
+      "child:2",
+      "child:3",
+      "child:4",
+      "child:5",
+      "child:6",
+    ],
+    edges: [
+      ["parent:0", "child:1"],
+      ["child:1", "child:2"],
+      ["child:2", "child:3"],
+      ["parent:0", "child:4"],
+      ["child:4", "child:5"],
+      ["child:5", "child:6"],
+    ],
+  });
 });
