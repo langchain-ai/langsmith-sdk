12dba643613e44d4160eb528023293504f88df6d
Tat Dat Duong <david@duong.cz>
2024-05-08 18:42:13 +0200

    Send comparative ID as well

--- js/src/evaluation/string_evaluator.ts
+++ js/src/evaluation/string_evaluator.ts
@@ -1,4 +1,3 @@
-import { RunTreeConfig } from "../run_trees.js";
 import { Example, Run, ScoreType, ValueType } from "../schemas.js";
 import { EvaluationResult, RunEvaluator } from "./evaluator.js";
 
