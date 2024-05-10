2efbc8f1a9da1fe2c129e6d0c6226b3b676d5314
Tat Dat Duong <david@duong.cz>
2024-05-10 21:13:20 +0200

    Fix lint

--- js/src/tests/traceable.test.ts
+++ js/src/tests/traceable.test.ts
@@ -1,5 +1,5 @@
 import type { RunTree, RunTreeConfig } from "../run_trees.js";
-import { ROOT, getCurrentRunTree, traceable } from "../traceable.js";
+import { ROOT, traceable } from "../traceable.js";
 import { getAssumedTreeFromCalls } from "./utils/tree.js";
 import { mockClient } from "./utils/mock_client.js";
 import { FakeChatModel } from "@langchain/core/utils/testing";
