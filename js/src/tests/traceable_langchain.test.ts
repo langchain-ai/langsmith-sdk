821d59c06544e8f56fbc1aea509dee56218b21c9
Tat Dat Duong <david@duong.cz>
2024-05-15 17:09:54 +0200

    Add tests verifying stream and batch

--- js/src/tests/traceable_langchain.test.ts
+++ js/src/tests/traceable_langchain.test.ts
@@ -9,9 +9,7 @@ import { BaseMessage, HumanMessage } from "@langchain/core/messages";
 import { awaitAllCallbacks } from "@langchain/core/callbacks/promises";
 import { RunnableTraceable, getLangchainCallbacks } from "../langchain.js";
 
-test("explicit traceable to langchain", async () => {
-  const { client, callSpy } = mockClient();
-
+describe("to langchain", () => {
   const llm = new FakeChatModel({});
   const prompt = ChatPromptTemplate.fromMessages<{ text: string }>([
     ["human", "{text}"],
@@ -19,49 +17,133 @@ test("explicit traceable to langchain", async () => {
   const parser = new StringOutputParser();
   const chain = prompt.pipe(llm).pipe(parser);
 
-  const main = traceable(
-    async (input: { text: string }) => {
-      return chain.invoke(input, {
-        callbacks: await getLangchainCallbacks(),
-      });
-    },
-    {
-      name: "main",
-      client,
-      tracingEnabled: true,
-      tags: ["welcome"],
-      metadata: { hello: "world" },
-    }
-  );
+  test("invoke", async () => {
+    const { client, callSpy } = mockClient();
 
-  const result = await main({ text: "Hello world" });
-  expect(result).toEqual("Hello world");
-
-  expect(getAssumedTreeFromCalls(callSpy.mock.calls)).toMatchObject({
-    nodes: [
-      "main:0",
-      "RunnableSequence:1",
-      "ChatPromptTemplate:2",
-      "FakeChatModel:3",
-      "StrOutputParser:4",
-    ],
-    edges: [
-      ["main:0", "RunnableSequence:1"],
-      ["RunnableSequence:1", "ChatPromptTemplate:2"],
-      ["RunnableSequence:1", "FakeChatModel:3"],
-      ["RunnableSequence:1", "StrOutputParser:4"],
-    ],
-    data: {
-      "main:0": {
-        inputs: { text: "Hello world" },
-        outputs: { outputs: "Hello world" },
-        tags: ["welcome"],
-        extra: { metadata: { hello: "world" } },
+    const main = traceable(
+      async (input: { text: string }) => {
+        return chain.invoke(input, {
+          callbacks: await getLangchainCallbacks(),
+        });
       },
-    },
+      {
+        name: "main",
+        client,
+        tracingEnabled: true,
+        tags: ["welcome"],
+        metadata: { hello: "world" },
+      }
+    );
+
+    const result = await main({ text: "Hello world" });
+    expect(result).toEqual("Hello world");
+
+    expect(getAssumedTreeFromCalls(callSpy.mock.calls)).toMatchObject({
+      nodes: [
+        "main:0",
+        "RunnableSequence:1",
+        "ChatPromptTemplate:2",
+        "FakeChatModel:3",
+        "StrOutputParser:4",
+      ],
+      edges: [
+        ["main:0", "RunnableSequence:1"],
+        ["RunnableSequence:1", "ChatPromptTemplate:2"],
+        ["RunnableSequence:1", "FakeChatModel:3"],
+        ["RunnableSequence:1", "StrOutputParser:4"],
+      ],
+      data: {
+        "main:0": {
+          inputs: { text: "Hello world" },
+          outputs: { outputs: "Hello world" },
+          tags: ["welcome"],
+          extra: { metadata: { hello: "world" } },
+        },
+      },
+    });
   });
 
-  test("explicit langchain to traceable", async () => {
+  test("to langchain stream", async () => {
+    const { client, callSpy } = mockClient();
+
+    const main = traceable(
+      async function* main(input: { text: string }) {
+        for await (const token of await chain.stream(input, {
+          callbacks: await getLangchainCallbacks(),
+        })) {
+          yield token;
+        }
+      },
+      { client, tracingEnabled: true }
+    );
+
+    const result = [];
+    for await (const token of main({ text: "Hello world" })) {
+      result.push(token);
+    }
+
+    expect(result).toEqual(["Hello world"]);
+    expect(getAssumedTreeFromCalls(callSpy.mock.calls)).toMatchObject({
+      nodes: [
+        "main:0",
+        "RunnableSequence:1",
+        "ChatPromptTemplate:2",
+        "FakeChatModel:3",
+        "StrOutputParser:4",
+      ],
+      edges: [
+        ["main:0", "RunnableSequence:1"],
+        ["RunnableSequence:1", "ChatPromptTemplate:2"],
+        ["RunnableSequence:1", "FakeChatModel:3"],
+        ["RunnableSequence:1", "StrOutputParser:4"],
+      ],
+    });
+  });
+
+  test("to langchain batch", async () => {
+    const { client, callSpy } = mockClient();
+
+    const main = traceable(
+      async (input: { texts: string[] }) => {
+        return chain.batch(
+          input.texts.map((text) => ({ text })),
+          { callbacks: await getLangchainCallbacks() }
+        );
+      },
+      { name: "main", client, tracingEnabled: true }
+    );
+
+    const result = await main({ texts: ["Hello world", "Who are you?"] });
+
+    expect(result).toEqual(["Hello world", "Who are you?"]);
+    expect(getAssumedTreeFromCalls(callSpy.mock.calls)).toMatchObject({
+      nodes: [
+        "main:0",
+        "RunnableSequence:1",
+        "RunnableSequence:2",
+        "ChatPromptTemplate:3",
+        "ChatPromptTemplate:4",
+        "FakeChatModel:5",
+        "FakeChatModel:6",
+        "StrOutputParser:7",
+        "StrOutputParser:8",
+      ],
+      edges: [
+        ["main:0", "RunnableSequence:1"],
+        ["main:0", "RunnableSequence:2"],
+        ["RunnableSequence:1", "ChatPromptTemplate:3"],
+        ["RunnableSequence:2", "ChatPromptTemplate:4"],
+        ["RunnableSequence:1", "FakeChatModel:5"],
+        ["RunnableSequence:2", "FakeChatModel:6"],
+        ["RunnableSequence:1", "StrOutputParser:7"],
+        ["RunnableSequence:2", "StrOutputParser:8"],
+      ],
+    });
+  });
+});
+
+describe("to traceable", () => {
+  test("invoke", async () => {
     const { client, callSpy } = mockClient();
 
     const llm = new FakeChatModel({});
@@ -109,88 +191,85 @@ test("explicit traceable to langchain", async () => {
       ],
     });
   });
+});
 
-  test("explicit nested", async () => {
-    const { client, callSpy } = mockClient();
+test("explicit nested", async () => {
+  const { client, callSpy } = mockClient();
 
-    const llm = new FakeChatModel({});
-    const prompt = ChatPromptTemplate.fromMessages<{ text: string }>([
-      ["human", "{text}"],
-    ]);
-    const parser = new StringOutputParser();
-    const chain = prompt
-      .pipe(llm)
-      .pipe(parser)
-      .withConfig({ runName: "chain" });
+  const llm = new FakeChatModel({});
+  const prompt = ChatPromptTemplate.fromMessages<{ text: string }>([
+    ["human", "{text}"],
+  ]);
+  const parser = new StringOutputParser();
+  const chain = prompt.pipe(llm).pipe(parser).withConfig({ runName: "chain" });
 
-    const wrappedModel = new RunnableTraceable({
-      func: traceable(
-        async (value: { input: string }) => {
-          const callbacks = await getLangchainCallbacks();
+  const wrappedModel = new RunnableTraceable({
+    func: traceable(
+      async (value: { input: string }) => {
+        const callbacks = await getLangchainCallbacks();
 
-          return chain.invoke(
-            { text: `Wrapped input: ${value.input}` },
-            { callbacks }
-          );
-        },
-        { name: "wrappedModel" }
-      ),
-    });
-
-    const main = traceable(
-      async () => {
-        return {
-          response: [
-            await wrappedModel.invoke(
-              { input: "Are you ready?" },
-              { callbacks: await getLangchainCallbacks() }
-            ),
-            await wrappedModel.invoke(
-              { input: "I said, Are. You. Ready?" },
-              { callbacks: await getLangchainCallbacks() }
-            ),
-          ],
-        };
+        return chain.invoke(
+          { text: `Wrapped input: ${value.input}` },
+          { callbacks }
+        );
       },
-      { name: "main", client, tracingEnabled: true }
-    );
+      { name: "wrappedModel" }
+    ),
+  });
 
-    const result = await main();
-    await awaitAllCallbacks();
+  const main = traceable(
+    async () => {
+      return {
+        response: [
+          await wrappedModel.invoke(
+            { input: "Are you ready?" },
+            { callbacks: await getLangchainCallbacks() }
+          ),
+          await wrappedModel.invoke(
+            { input: "I said, Are. You. Ready?" },
+            { callbacks: await getLangchainCallbacks() }
+          ),
+        ],
+      };
+    },
+    { name: "main", client, tracingEnabled: true }
+  );
 
-    expect(result).toEqual({
-      response: [
-        "Wrapped input: Are you ready?",
-        "Wrapped input: I said, Are. You. Ready?",
-      ],
-    });
+  const result = await main();
+  await awaitAllCallbacks();
 
-    expect(getAssumedTreeFromCalls(callSpy.mock.calls)).toMatchObject({
-      nodes: [
-        "main:0",
-        "wrappedModel:1",
-        "chain:2",
-        "ChatPromptTemplate:3",
-        "FakeChatModel:4",
-        "StrOutputParser:5",
-        "wrappedModel:6",
-        "chain:7",
-        "ChatPromptTemplate:8",
-        "FakeChatModel:9",
-        "StrOutputParser:10",
-      ],
-      edges: [
-        ["main:0", "wrappedModel:1"],
-        ["wrappedModel:1", "chain:2"],
-        ["chain:2", "ChatPromptTemplate:3"],
-        ["chain:2", "FakeChatModel:4"],
-        ["chain:2", "StrOutputParser:5"],
-        ["main:0", "wrappedModel:6"],
-        ["wrappedModel:6", "chain:7"],
-        ["chain:7", "ChatPromptTemplate:8"],
-        ["chain:7", "FakeChatModel:9"],
-        ["chain:7", "StrOutputParser:10"],
-      ],
-    });
+  expect(result).toEqual({
+    response: [
+      "Wrapped input: Are you ready?",
+      "Wrapped input: I said, Are. You. Ready?",
+    ],
+  });
+
+  expect(getAssumedTreeFromCalls(callSpy.mock.calls)).toMatchObject({
+    nodes: [
+      "main:0",
+      "wrappedModel:1",
+      "chain:2",
+      "ChatPromptTemplate:3",
+      "FakeChatModel:4",
+      "StrOutputParser:5",
+      "wrappedModel:6",
+      "chain:7",
+      "ChatPromptTemplate:8",
+      "FakeChatModel:9",
+      "StrOutputParser:10",
+    ],
+    edges: [
+      ["main:0", "wrappedModel:1"],
+      ["wrappedModel:1", "chain:2"],
+      ["chain:2", "ChatPromptTemplate:3"],
+      ["chain:2", "FakeChatModel:4"],
+      ["chain:2", "StrOutputParser:5"],
+      ["main:0", "wrappedModel:6"],
+      ["wrappedModel:6", "chain:7"],
+      ["chain:7", "ChatPromptTemplate:8"],
+      ["chain:7", "FakeChatModel:9"],
+      ["chain:7", "StrOutputParser:10"],
+    ],
   });
 });
