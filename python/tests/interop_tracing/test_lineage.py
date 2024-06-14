128405356695fe9bc065227c3f91167555be6965
Eugene Yurtsev <eyurtsev@gmail.com>
2024-06-14 17:27:31 -0400

    x

---
+++ python/tests/interop_tracing/test_lineage.py
@@ -0,0 +1,164 @@
+import asyncio
+from concurrent import futures
+from unittest.mock import MagicMock
+
+import pytest
+
+from langsmith import Client
+from langsmith import traceable
+from langsmith.run_helpers import tracing_context
+from tests.interop_tracing.utils import extract_span_tree
+
+
+def _get_mock_client() -> Client:
+    mock_session = MagicMock()
+    client = Client(session=mock_session, api_key="test")
+    return client
+
+
+@pytest.fixture
+def mock_client() -> Client:
+    return _get_mock_client()
+
+
+def test_simple_lineage(mock_client: Client) -> None:
+    """Test that the tracing context is enabled"""
+    with tracing_context(enabled=True):
+
+        @traceable(client=mock_client)
+        def child(x: int):
+            return x
+
+        @traceable(client=mock_client)
+        def parent(x: int):
+            return child(x + 1)
+
+        @traceable(client=mock_client)
+        def grand_parent(x: int):
+            return parent(x + 1)
+
+        assert grand_parent(1) == 3
+
+    tree = extract_span_tree(mock_client)
+    assert len(tree.get_root_nodes()) == 1
+    sorted_spans = tree.get_breadth_first_travel()
+    assert len(sorted_spans) == 3
+    names = [span["name"] for span, _ in sorted_spans]
+    assert names == ["grand_parent", "parent", "child"]
+
+
+async def test_async_simple_lineage(mock_client: Client) -> None:
+    """Test that the tracing context is enabled"""
+    with tracing_context(enabled=True):
+
+        @traceable(client=mock_client)
+        async def child(x: int):
+            return x
+
+        @traceable(client=mock_client)
+        async def parent(x: int):
+            return await child(x + 1)
+
+        @traceable(client=mock_client)
+        async def grand_parent(x: int):
+            return await parent(x + 1)
+
+        assert await grand_parent(1) == 3
+
+    tree = extract_span_tree(mock_client)
+    assert len(tree.get_root_nodes()) == 1
+    sorted_spans = tree.get_breadth_first_travel()
+    assert len(sorted_spans) == 3
+    names = [span["name"] for span, _ in sorted_spans]
+    assert names == ["grand_parent", "parent", "child"]
+
+
+@pytest.mark.xfail(reason="Unclear if this is supported.")
+async def test_async_sync_simple_lineage(mock_client: Client) -> None:
+    """Test invoking sync code from async and vice versa."""
+    with tracing_context(enabled=True):
+
+        @traceable(client=mock_client)
+        async def child(x: int):
+            return x
+
+        @traceable(client=mock_client)
+        def parent(x: int):
+            # Invoke on a separate thread
+
+            with futures.ThreadPoolExecutor() as executor:
+                future = executor.submit(asyncio.run, child(x + 1))
+                return future.result()
+
+        @traceable(client=mock_client)
+        async def grand_parent(x: int):
+            return parent(x + 1)
+
+        assert await grand_parent(1) == 3
+
+    tree = extract_span_tree(mock_client)
+    assert len(tree.get_root_nodes()) == 1
+    sorted_spans = tree.get_breadth_first_travel()
+    assert len(sorted_spans) == 3
+    names = [span["name"] for span, _ in sorted_spans]
+    assert names == ["grand_parent", "parent", "child"]
+
+
+def test_recursive_depth_10(mock_client: Client) -> None:
+    """Test recursive invocation."""
+    with tracing_context(enabled=True):
+
+        @traceable(client=mock_client)
+        def recursive(x: int):
+            if x == 0:
+                return 0
+            return recursive(x - 1)
+
+        assert recursive(10) == 0
+
+    tree = extract_span_tree(mock_client)
+    assert len(tree.get_root_nodes()) == 1
+    sorted_spans = tree.get_breadth_first_travel()
+    assert len(sorted_spans) == 11
+    names = [span["name"] for span, _ in sorted_spans]
+    assert names == ["recursive"] * 11
+
+
+async def test_async_recursive_depth_10(mock_client: Client) -> None:
+    """Test recursive invocation."""
+    with tracing_context(enabled=True):
+
+        @traceable(client=mock_client)
+        async def recursive(x: int):
+            if x == 0:
+                return 0
+            return await recursive(x - 1)
+
+        assert await recursive(10) == 0
+
+    tree = extract_span_tree(mock_client)
+    assert len(tree.get_root_nodes()) == 1
+    sorted_spans = tree.get_breadth_first_travel()
+    assert len(sorted_spans) == 11
+    names = [span["name"] for span, _ in sorted_spans]
+    assert names == ["recursive"] * 11
+
+
+async def test_fibonacci(mock_client: Client) -> None:
+    """Test recursive invocation."""
+    with tracing_context(enabled=True):
+
+        @traceable(client=mock_client)
+        async def fib(n: int):
+            if n <= 1:
+                return n
+            return await fib(n - 1) + await fib(n - 2)
+
+        assert await fib(10) == 55
+
+    tree = extract_span_tree(mock_client)
+
+    sorted_spans = tree.get_breadth_first_travel()
+    assert len(sorted_spans) == 177
+    names = [span["name"] for span, _ in sorted_spans]
+    assert names == ["fib"] * 177
