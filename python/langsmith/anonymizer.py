fee94fb8464c152718ab43ce40bcbac7ce369c5b
Tat Dat Duong <david@duong.cz>
2024-06-14 16:54:27 +0200

    Add tests

--- python/langsmith/anonymizer.py
+++ python/langsmith/anonymizer.py
@@ -1,11 +1,11 @@
-import copy
+import copy  # noqa
 import re
 from abc import abstractmethod
 from collections import defaultdict
-from typing import Any, Callable, List, Optional, Tuple, TypedDict, Union
+from typing import Any, Callable, List, Optional, Tuple, TypedDict, TypeVar, Union
 
 
-class ExtractOptions(TypedDict):
+class _ExtractOptions(TypedDict):
     maxDepth: Optional[int]
     """
     Maximum depth to traverse to to extract string nodes
@@ -13,12 +13,17 @@ class ExtractOptions(TypedDict):
 
 
 class StringNode(TypedDict):
+    """String node extracted from the data."""
+
     value: str
+    """String value."""
+
     path: tuple[str | int]
+    """Path to the string node in the data."""
 
 
-def _extract_string_nodes(data: Any, options: ExtractOptions) -> List[StringNode]:
-    parsed_options = {**options, "maxDepth": options.get("maxDepth", 10)}
+def _extract_string_nodes(data: Any, options: _ExtractOptions) -> List[StringNode]:
+    max_depth = options.get("maxDepth", 10)
 
     queue: List[Tuple[Any, int, tuple[str | int]]] = [(data, 0, tuple())]
     result: List[StringNode] = []
@@ -30,46 +35,63 @@ def _extract_string_nodes(data: Any, options: ExtractOptions) -> List[StringNode
         value, depth, path = task
 
         if isinstance(value, dict) or isinstance(value, defaultdict):
-            if depth >= parsed_options["maxDepth"]:
+            if depth >= max_depth:
                 continue
             for key, nested_value in value.items():
                 queue.append((nested_value, depth + 1, path + (key,)))
         elif isinstance(value, list):
-            if depth >= parsed_options["maxDepth"]:
+            if depth >= max_depth:
                 continue
             for i, item in enumerate(value):
                 queue.append((item, depth + 1, path + (i,)))
         elif isinstance(value, str):
-            result.append(StringNode(value, path))
+            result.append(StringNode(value=value, path=path))
 
     return result
 
 
 class StringNodeProcessor:
+    """Processes a list of string nodes for masking."""
+
     @abstractmethod
     def mask_nodes(self, nodes: List[StringNode]) -> List[StringNode]:
-        """Mask node."""
+        """Accept and return a list of string nodes to be masked."""
 
 
 class ReplacerOptions(TypedDict):
+    """Configuration options for replacing sensitive data."""
+
     maxDepth: Optional[int]
+    """Maximum depth to traverse to to extract string nodes."""
+
     deepClone: Optional[bool]
+    """Deep clone the data before replacing."""
 
 
 class StringNodeRule(TypedDict):
+    """Declarative rule used for replacing sensitive data."""
+
     pattern: Union[str, re.Pattern]
+    """Regex pattern to match."""
+
     replace: Optional[str] = "[redacted]"
+    """Replacement value. Defaults to `[redacted]` if not specified."""
 
 
 ReplacerType = Union[
-    Callable[[str, Optional[str]], str], List[StringNodeRule], StringNodeProcessor
+    Callable[[str, tuple[str | int]], str], List[StringNodeRule], StringNodeProcessor
 ]
+
+T = TypeVar("T", str, dict)
 
 
 def replace_sensitive_data(
-    data: Any, replacer: ReplacerType, options: Optional[ReplacerOptions] = None
-) -> Any:
-    nodes = _extract_string_nodes(data, options)
+    data: T, replacer: ReplacerType, options: Optional[ReplacerOptions] = None
+) -> T:
+    """Replace sensitive data."""
+    nodes = _extract_string_nodes(
+        data, {"maxDepth": (options or {}).get("maxDepth", 10)}
+    )
     mutate_value = copy.deepcopy(data) if options and options["deepClone"] else data
 
     if isinstance(replacer, list):
@@ -77,11 +99,11 @@ def replace_sensitive_data(
         def mask_nodes(nodes: List[StringNode]) -> List[StringNode]:
             result = []
             for item in nodes:
-                new_value = item.value
+                new_value = item["value"]
                 for rule in replacer:
                     new_value = rule["pattern"].sub(rule["replace"], new_value)
-                if new_value != item.value:
-                    result.append(StringNode(new_value, item.path))
+                if new_value != item["value"]:
+                    result.append(StringNode(value=new_value, path=item["path"]))
             return result
 
         processor = StringNodeProcessor()
@@ -89,11 +111,12 @@ def replace_sensitive_data(
     elif callable(replacer):
 
         def mask_nodes(nodes: List[StringNode]) -> List[StringNode]:
-            return [
-                StringNode(replacer(node.value, node.path), node.path)
-                for node in nodes
-                if replacer(node.value, node.path) != node.value
-            ]
+            retval: list[StringNode] = []
+            for node in nodes:
+                candidate = replacer(node["value"], node["path"])
+                if candidate != node["value"]:
+                    retval.append(StringNode(value=candidate, path=node["path"]))
+            return retval
 
         processor = StringNodeProcessor()
         processor.mask_nodes = mask_nodes
@@ -102,14 +125,14 @@ def replace_sensitive_data(
 
     to_update = processor.mask_nodes(nodes)
     for node in to_update:
-        if not node.path:
-            mutate_value = node.value
+        if not node["path"]:
+            mutate_value = node["value"]
         else:
             temp = mutate_value
-            for part in node.path[:-1]:
+            for part in node["path"][:-1]:
                 temp = temp[part]
 
-            last_part = node.path[-1]
-            temp[last_part] = node.value
+            last_part = node["path"][-1]
+            temp[last_part] = node["value"]
 
     return mutate_value
---
+++ python/tests/unit_tests/test_anonymizer.py
@@ -0,0 +1,58 @@
+import re
+from uuid import uuid4
+
+from langsmith.anonymizer import StringNodeRule, replace_sensitive_data
+
+EMAIL_REGEX = re.compile(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}")
+UUID_REGEX = re.compile(
+    r"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
+)
+
+
+def test_replacer_function():
+    def replacer(text: str, _: tuple[str | int]):
+        text = EMAIL_REGEX.sub("[email address]", text)
+        text = UUID_REGEX.sub("[uuid]", text)
+        return text
+
+    assert replace_sensitive_data(
+        {
+            "message": "Hello, this is my email: hello@example.com",
+            "metadata": str(uuid4()),
+        },
+        replacer,
+    ) == {
+        "message": "Hello, this is my email: [email address]",
+        "metadata": "[uuid]",
+    }
+
+    assert replace_sensitive_data(["human", "hello@example.com"], replacer) == [
+        "human",
+        "[email address]",
+    ]
+    assert replace_sensitive_data("hello@example.com", replacer) == "[email address]"
+
+
+def test_replacer_declared():
+    replacers = [
+        StringNodeRule(pattern=EMAIL_REGEX, replace="[email address]"),
+        StringNodeRule(pattern=UUID_REGEX, replace="[uuid]"),
+    ]
+
+    assert replace_sensitive_data(
+        {
+            "message": "Hello, this is my email: hello@example.com",
+            "metadata": str(uuid4()),
+        },
+        replacers,
+    ) == {
+        "message": "Hello, this is my email: [email address]",
+        "metadata": "[uuid]",
+    }
+
+    assert replace_sensitive_data(["human", "hello@example.com"], replacers) == [
+        "human",
+        "[email address]",
+    ]
+
+    assert replace_sensitive_data("hello@example.com", replacers) == "[email address]"
