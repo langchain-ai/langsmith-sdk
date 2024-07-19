ad2fe10c8fa5dec5c58ce40ee681da42c827a4fa
Tat Dat Duong <david@duong.cz>
2024-07-18 20:18:35 -0700

    fix(test): create a new dataset per test invocation to fix parallel PRs

--- js/src/tests/evaluate.int.test.ts
+++ js/src/tests/evaluate.int.test.ts
@@ -8,60 +8,84 @@ import { Client } from "../index.js";
 import { afterAll, beforeAll } from "@jest/globals";
 import { RunnableLambda, RunnableSequence } from "@langchain/core/runnables";
 
-const TESTING_DATASET_NAME = "test_dataset_js_evaluate_123";
+const TESTING_DATASET_NAME = `test_dataset_js_evaluate_${new Date().toISOString()}`;
+const SPLITS_DATASET_NAME = `my_splits_ds2_${new Date().toISOString()}`;
+const client = new Client();
+
+type InputsType = { input: number };
+type OutputsType = { output: number };
 
 beforeAll(async () => {
-  const client = new Client();
   if (!(await client.hasDataset({ datasetName: TESTING_DATASET_NAME }))) {
+    console.log(`Creating dataset ${TESTING_DATASET_NAME}`);
+
     // create a new dataset
     await client.createDataset(TESTING_DATASET_NAME, {
       description:
         "For testing purposed. Is created & deleted for each test run.",
     });
-    // create examples
+
     const res = await client.createExamples({
-      inputs: [{ input: 1 }, { input: 2 }],
-      outputs: [{ output: 2 }, { output: 3 }],
+      inputs: [{ input: 1 }, { input: 2 }] satisfies InputsType[],
+      outputs: [{ output: 2 }, { output: 3 }] satisfies OutputsType[],
       datasetName: TESTING_DATASET_NAME,
     });
-    if (res.length !== 2) {
-      throw new Error("Failed to create examples");
-    }
+
+    if (res.length !== 2)
+      throw new Error(`Failed to create examples for ${TESTING_DATASET_NAME}`);
+  }
+
+  if (!(await client.hasDataset({ datasetName: SPLITS_DATASET_NAME }))) {
+    console.log(`Creating dataset ${SPLITS_DATASET_NAME}`);
+
+    await client.createDataset(SPLITS_DATASET_NAME, {
+      description:
+        "For testing purposed. Is created & deleted for each test run.",
+    });
+
+    const res = await client.createExamples({
+      inputs: [{ input: 1 }, { input: 2 }, { input: 3 }] satisfies InputsType[],
+      outputs: [
+        { output: 2 },
+        { output: 3 },
+        { output: 4 },
+      ] satisfies OutputsType[],
+      splits: [["test"], ["train"], ["validation", "test"]],
+      datasetName: SPLITS_DATASET_NAME,
+    });
+
+    if (res.length !== 3)
+      throw new Error(`Failed to create examples for ${SPLITS_DATASET_NAME}`);
   }
 });
 
 afterAll(async () => {
-  const client = new Client();
-  await client.deleteDataset({
-    datasetName: TESTING_DATASET_NAME,
-  });
-  try {
-    await client.deleteDataset({
-      datasetName: "my_splits_ds2",
-    });
-  } catch {
-    //pass
-  }
+  await Promise.allSettled([
+    (() => {
+      console.log(`Deleting dataset ${TESTING_DATASET_NAME}`);
+      return client.deleteDataset({ datasetName: TESTING_DATASET_NAME });
+    })(),
+    (() => {
+      console.log(`Deleting dataset ${SPLITS_DATASET_NAME}`);
+      return client.deleteDataset({ datasetName: SPLITS_DATASET_NAME });
+    })(),
+  ]);
 });
 
 test("evaluate can evaluate", async () => {
-  const targetFunc = (input: Record<string, any>) => {
-    console.log("__input__", input);
-    return {
-      foo: input.input + 1,
-    };
-  };
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
 
   const evalRes = await evaluate(targetFunc, {
     data: TESTING_DATASET_NAME,
     description: "Experiment from evaluate can evaluate integration test",
   });
-  // console.log(evalRes.results)
+
   expect(evalRes.results).toHaveLength(2);
 
   expect(evalRes.results[0].run).toBeDefined();
   expect(evalRes.results[0].example).toBeDefined();
   expect(evalRes.results[0].evaluationResults).toBeDefined();
+
   const firstRun = evalRes.results[0].run;
   // The examples are not always in the same order, so it should always be 2 or 3
   expect(firstRun.outputs?.foo).toBeGreaterThanOrEqual(2);
@@ -73,6 +97,7 @@ test("evaluate can evaluate", async () => {
   expect(evalRes.results[1].run).toBeDefined();
   expect(evalRes.results[1].example).toBeDefined();
   expect(evalRes.results[1].evaluationResults).toBeDefined();
+
   const secondRun = evalRes.results[1].run;
   // The examples are not always in the same order, so it should always be 2 or 3
   expect(secondRun.outputs?.foo).toBeGreaterThanOrEqual(2);
@@ -83,12 +108,7 @@ test("evaluate can evaluate", async () => {
 });
 
 test("evaluate can repeat", async () => {
-  const targetFunc = (input: Record<string, any>) => {
-    console.log("__input__", input);
-    return {
-      foo: input.input + 1,
-    };
-  };
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
 
   const evalRes = await evaluate(targetFunc, {
     data: TESTING_DATASET_NAME,
@@ -183,12 +203,7 @@ test("evaluate can evaluate with RunEvaluator evaluators", async () => {
 });
 
 test("evaluate can evaluate with custom evaluators", async () => {
-  const targetFunc = (input: Record<string, any>) => {
-    console.log("__input__", input);
-    return {
-      foo: input.input + 1,
-    };
-  };
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
 
   const customEvaluator = (run: Run, example?: Example) => {
     return Promise.resolve({
@@ -255,12 +270,7 @@ test("evaluate can evaluate with custom evaluators", async () => {
 });
 
 test("evaluate can evaluate with summary evaluators", async () => {
-  const targetFunc = (input: Record<string, any>) => {
-    console.log("__input__", input);
-    return {
-      foo: input.input + 1,
-    };
-  };
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
 
   const customSummaryEvaluator = (
     runs: Run[],
@@ -313,13 +323,7 @@ test("evaluate can evaluate with summary evaluators", async () => {
 });
 
 test.skip("can iterate over evaluate results", async () => {
-  const targetFunc = (input: Record<string, any>) => {
-    console.log("__input__", input);
-    return {
-      foo: input.input + 1,
-    };
-  };
-
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
   const customEvaluator = async (run: Run, example?: Example) => {
     return Promise.resolve({
       key: "key",
@@ -342,12 +346,7 @@ test.skip("can iterate over evaluate results", async () => {
 });
 
 test("can pass multiple evaluators", async () => {
-  const targetFunc = (input: Record<string, any>) => {
-    console.log("__input__", input);
-    return {
-      foo: input.input + 1,
-    };
-  };
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
 
   const customEvaluatorOne = async (run: Run, example?: Example) => {
     return Promise.resolve({
@@ -389,32 +388,16 @@ test("can pass multiple evaluators", async () => {
 });
 
 test("split info saved correctly", async () => {
-  const client = new Client();
-  // create a new dataset
-  await client.createDataset("my_splits_ds2", {
-    description:
-      "For testing purposed. Is created & deleted for each test run.",
-  });
-  // create examples
-  await client.createExamples({
-    inputs: [{ input: 1 }, { input: 2 }, { input: 3 }],
-    outputs: [{ output: 2 }, { output: 3 }, { output: 4 }],
-    splits: [["test"], ["train"], ["validation", "test"]],
-    datasetName: "my_splits_ds2",
-  });
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
 
-  const targetFunc = (input: Record<string, any>) => {
-    console.log("__input__", input);
-    return {
-      foo: input.input + 1,
-    };
-  };
   await evaluate(targetFunc, {
-    data: client.listExamples({ datasetName: "my_splits_ds2" }),
+    data: client.listExamples({ datasetName: SPLITS_DATASET_NAME }),
     description: "splits info saved correctly",
   });
 
-  const exp = client.listProjects({ referenceDatasetName: "my_splits_ds2" });
+  const exp = client.listProjects({
+    referenceDatasetName: SPLITS_DATASET_NAME,
+  });
   let myExp: TracerSession | null = null;
   for await (const session of exp) {
     myExp = session;
@@ -425,13 +408,15 @@ test("split info saved correctly", async () => {
 
   await evaluate(targetFunc, {
     data: client.listExamples({
-      datasetName: "my_splits_ds2",
+      datasetName: SPLITS_DATASET_NAME,
       splits: ["test"],
     }),
     description: "splits info saved correctly",
   });
 
-  const exp2 = client.listProjects({ referenceDatasetName: "my_splits_ds2" });
+  const exp2 = client.listProjects({
+    referenceDatasetName: SPLITS_DATASET_NAME,
+  });
   let myExp2: TracerSession | null = null;
   for await (const session of exp2) {
     if (myExp2 === null || session.start_time > myExp2.start_time) {
@@ -445,13 +430,15 @@ test("split info saved correctly", async () => {
 
   await evaluate(targetFunc, {
     data: client.listExamples({
-      datasetName: "my_splits_ds2",
+      datasetName: SPLITS_DATASET_NAME,
       splits: ["train"],
     }),
     description: "splits info saved correctly",
   });
 
-  const exp3 = client.listProjects({ referenceDatasetName: "my_splits_ds2" });
+  const exp3 = client.listProjects({
+    referenceDatasetName: SPLITS_DATASET_NAME,
+  });
   let myExp3: TracerSession | null = null;
   for await (const session of exp3) {
     if (myExp3 === null || session.start_time > myExp3.start_time) {
@@ -465,12 +452,7 @@ test("split info saved correctly", async () => {
 });
 
 test("can pass multiple summary evaluators", async () => {
-  const targetFunc = (input: Record<string, any>) => {
-    console.log("__input__", input);
-    return {
-      foo: input.input + 1,
-    };
-  };
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
 
   const customSummaryEvaluator = (
     runs: Run[],
@@ -511,17 +493,11 @@ test("can pass multiple summary evaluators", async () => {
 });
 
 test("can pass AsyncIterable of Example's to evaluator instead of dataset name", async () => {
-  const client = new Client();
   const examplesIterator = client.listExamples({
     datasetName: TESTING_DATASET_NAME,
   });
 
-  const targetFunc = (input: Record<string, any>) => {
-    console.log("__input__", input);
-    return {
-      foo: input.input + 1,
-    };
-  };
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
 
   const customEvaluator = (run: Run, example?: Example) => {
     return Promise.resolve({
@@ -550,12 +526,7 @@ test("can pass AsyncIterable of Example's to evaluator instead of dataset name",
 });
 
 test("max concurrency works with custom evaluators", async () => {
-  const targetFunc = (input: Record<string, any>) => {
-    console.log("__input__", input);
-    return {
-      foo: input.input + 1,
-    };
-  };
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
 
   const customEvaluator = (run: Run, example?: Example) => {
     return Promise.resolve({
@@ -586,12 +557,7 @@ test("max concurrency works with custom evaluators", async () => {
 });
 
 test("max concurrency works with summary evaluators", async () => {
-  const targetFunc = (input: Record<string, any>) => {
-    console.log("__input__", input);
-    return {
-      foo: input.input + 1,
-    };
-  };
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
 
   const customSummaryEvaluator = (
     runs: Run[],
@@ -631,7 +597,7 @@ test("max concurrency works with summary evaluators", async () => {
 
 test("Target func can be a runnable", async () => {
   const targetFunc = RunnableSequence.from([
-    RunnableLambda.from((input: Record<string, any>) => ({
+    RunnableLambda.from((input: InputsType) => ({
       foo: input.input + 1,
     })).withConfig({ runName: "First Step" }),
     RunnableLambda.from((input: { foo: number }) => ({
@@ -694,7 +660,6 @@ test("Target func can be a runnable", async () => {
 });
 
 test("evaluate can accept array of examples", async () => {
-  const client = new Client();
   const examplesIterator = client.listExamples({
     datasetName: TESTING_DATASET_NAME,
   });
@@ -703,13 +668,7 @@ test("evaluate can accept array of examples", async () => {
     examples.push(example);
   }
 
-  const targetFunc = (input: Record<string, any>) => {
-    console.log("__input__", input);
-    return {
-      foo: input.input + 1,
-    };
-  };
-
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
   const customEvaluator = (run: Run, example?: Example) => {
     return Promise.resolve({
       key: "key",
@@ -737,10 +696,7 @@ test("evaluate can accept array of examples", async () => {
 });
 
 test("evaluate accepts evaluators which return multiple feedback keys", async () => {
-  const targetFunc = (input: Record<string, any>) => {
-    return { foo: input.input + 1 };
-  };
-
+  const targetFunc = (input: InputsType) => ({ foo: input.input + 1 });
   const customEvaluator = (
     run: Run,
     example?: Example
