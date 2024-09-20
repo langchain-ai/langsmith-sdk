ef092de59b90bedc1316b6ce9da5782e28caa8ef
William FH <13333726+hinthornw@users.noreply.github.com>
2024-09-19 17:38:33 -0700

    [Py] Add default_factory in run tree (#1023)
    For events.
    Needed before removing core's Run subclass

--- python/langsmith/run_trees.py
+++ python/langsmith/run_trees.py
@@ -58,6 +58,10 @@ class RunTree(ls_schemas.RunBase):
     )
     session_id: Optional[UUID] = Field(default=None, alias="project_id")
     extra: Dict = Field(default_factory=dict)
+    tags: Optional[List[str]] = Field(default_factory=list)
+    events: List[Dict] = Field(default_factory=list)
+    """List of events associated with the run, like
+    start and end events."""
     _client: Optional[Client] = None
     dotted_order: str = Field(
         default="", description="The order of the run in the tree."
