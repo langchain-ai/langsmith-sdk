08ec720e8f1de3d848aaed8656546646a3524c1f
Nuno Campos <nuno@langchain.dev>
2024-10-01 16:07:07 -0700

    Lint

--- python/langsmith/client.py
+++ python/langsmith/client.py
@@ -5825,10 +5825,10 @@ def _tracing_control_thread_func(client_ref: weakref.ref[Client]) -> None:
     size_limit: int = batch_ingest_config["size_limit"]
     scale_up_nthreads_limit: int = batch_ingest_config["scale_up_nthreads_limit"]
     scale_up_qsize_trigger: int = batch_ingest_config["scale_up_qsize_trigger"]
-    use_multipart: bool = os.getenv(
-        "LANGSMITH_FF_MULTIPART",
-        batch_ingest_config.get("use_multipart_endpoint", False),
-    )
+    if multipart_override := os.getenv("LANGSMITH_FF_MULTIPART"):
+        use_multipart = multipart_override.lower() in ["1", "true"]
+    else:
+        use_multipart = batch_ingest_config.get("use_multipart_endpoint", False)
 
     sub_threads: List[threading.Thread] = []
     # 1 for this func, 1 for getrefcount, 1 for _get_data_type_cached
