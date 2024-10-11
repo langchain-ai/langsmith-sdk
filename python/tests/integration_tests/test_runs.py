7c2cb5aacaca4eaecd3b4961db155f3c1f78e997
Nick Huang <nickhuang@Nicks-MacBook-Pro.local>
2024-10-11 13:42:28 -0400

    Clean up test hygiene

--- python/tests/integration_tests/test_runs.py
+++ python/tests/integration_tests/test_runs.py
@@ -459,24 +459,24 @@ async def test_async_generator_reduce_fn(langchain_client: Client):
 
 async def test_end_metadata_with_run_tree(langchain_client: Client):
     project_name = "__My Tracer Project - test_end_metadata_with_run_tree"
-    run_meta = uuid.uuid4().hex
+    run_id = uuid.uuid4()
 
     run_tree = RunTree(
         name="my_chain_run",
+        id=run_id,
         run_type="chain",
         project_name=project_name,
     )
 
-    run_tree.end(metadata={"final_metadata": run_meta}, outputs={"result": "success"})
+    run_tree.end(metadata={"final_metadata": run_id.hex}, outputs={"result": "success"})
     run_tree.post()
 
-    filter_ = (
-        f'and(eq(metadata_key, "final_metadata"), eq(metadata_value, "{run_meta}"))'
-    )
+    filter_ = f'eq(id, "{run_id}")'
     poll_runs_until_count(langchain_client, project_name, 1, filter_=filter_)
 
     runs_ = list(langchain_client.list_runs(project_name=project_name, filter=filter_))
     run = runs_[0]
     assert run.run_type == "chain"
-    assert run.metadata["final_metadata"] == run_meta
+    assert run.metadata["final_metadata"] == run_id.hex
     assert run.outputs == {"result": "success"}
+    langchain_client.delete_project(project_name)
