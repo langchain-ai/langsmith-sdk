bde1917d52fbba9ffc7e8e9ae8edc522893d83e4
Nick Huang <nickhuang@Nicks-MacBook-Pro.local>
2024-10-10 21:43:31 -0400

    Add JS implementation and add integration tests in Python and JS

--- js/src/run_trees.ts
+++ js/src/run_trees.ts
@@ -298,11 +298,20 @@ export class RunTree implements BaseRun {
   async end(
     outputs?: KVMap,
     error?: string,
-    endTime = Date.now()
+    endTime = Date.now(),
+    metadata?: KVMap
   ): Promise<void> {
     this.outputs = this.outputs ?? outputs;
     this.error = this.error ?? error;
     this.end_time = this.end_time ?? endTime;
+    this.extra = this.extra ? {
+      ...this.extra,
+      metadata: {
+        ...metadata
+      }
+    } : {
+      metadata: metadata
+    }
   }
 
   private _convertToCreate(
