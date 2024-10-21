5ba4b10481557fed918ca6870bbb58978e6b75b8
Ankush Gola <ankush.gola@gmail.com>
2024-10-21 15:13:07 -0700

    finish writing assertions

--- rust/crates/langsmith_tracing_client/tests/tracing_client_test.rs
+++ rust/crates/langsmith_tracing_client/tests/tracing_client_test.rs
@@ -1,6 +1,6 @@
 use langsmith_tracing_client::client::run::{
-    Attachment, RunCommon, RunCreate, RunCreateWithAttachments, RunUpdate,
-    RunUpdateWithAttachments, TimeValue, RunIO
+    Attachment, RunCommon, RunCreate, RunCreateExtended, RunIO, RunUpdate, RunUpdateExtended,
+    TimeValue,
 };
 use langsmith_tracing_client::client::tracing_client::{ClientConfig, TracingClient};
 use mockito::{Matcher, Server};
@@ -10,8 +10,8 @@ use std::time::Duration;
 // use tokio::io::AsyncWriteExt;
 use multipart::server::Multipart;
 use std::io::{self, Read, Write};
-use tempfile::TempDir;
 use std::sync::{Arc, Mutex};
+use tempfile::TempDir;
 
 #[derive(Debug)]
 struct MultipartField {
@@ -53,7 +53,8 @@ fn handle_request(body: Vec<u8>, content_type_str: String) -> Vec<MultipartField
 #[tokio::test]
 async fn test_tracing_client_submit_run_create() {
     let mut server = Server::new_async().await;
-    let captured_request: Arc<Mutex<(Vec<u8>, String)>> = Arc::new(Mutex::new((Vec::new(), String::new())));
+    let captured_request: Arc<Mutex<(Vec<u8>, String)>> =
+        Arc::new(Mutex::new((Vec::new(), String::new())));
     let captured_request_clone = Arc::clone(&captured_request);
 
     let m = server
@@ -90,25 +91,21 @@ async fn test_tracing_client_submit_run_create() {
     let mut test_file = File::create(&test_file_path).unwrap();
     writeln!(test_file, "Test file content for create").unwrap();
 
-    let mut attachments = HashMap::new();
-    attachments.insert(
-        "attachment_1".to_string(),
-        Attachment {
-            filename: "file1.txt".to_string(),
-            data: Some(vec![1, 2, 3]),
-            content_type: "application/octet-stream".to_string(),
-        },
-    );
-    attachments.insert(
-        "attachment_2".to_string(),
-        Attachment {
-            filename: test_file_path.into_os_string().into_string().unwrap(),
-            data: None, // this will cause the processor to read from disk
-            content_type: "text/plain".to_string(),
-        },
-    );
+    let mut attachments = Vec::new();
+    attachments.push(Attachment {
+        ref_name: "attachment_1".to_string(),
+        filename: "file1.txt".to_string(),
+        data: Some(vec![1, 2, 3]),
+        content_type: "application/octet-stream".to_string(),
+    });
+    attachments.push(Attachment {
+        ref_name: "attachment_2".to_string(),
+        filename: test_file_path.into_os_string().into_string().unwrap(),
+        data: None, // this will cause the processor to read from disk
+        content_type: "text/plain".to_string(),
+    });
 
-    let run_create = RunCreateWithAttachments {
+    let run_create = RunCreateExtended {
         run_create: RunCreate {
             common: RunCommon {
                 id: String::from("test_id"),
@@ -119,21 +116,21 @@ async fn test_tracing_client_submit_run_create() {
                 error: None,
                 serialized: Some(serde_json::json!({"key": "value"})),
                 events: serde_json::json!([{ "event": "event_data" }]),
-                tags: serde_json::json!({"tag": "value"}),
+                tags: serde_json::json!([{"tag": "value"}]),
                 session_id: None,
                 session_name: Some("Session Name".to_string()),
-                io: RunIO {
-                    inputs: serde_json::json!({"input": "value"}),
-                    outputs: Some(serde_json::json!({"output": "value"})),
-                }
             },
             name: String::from("Run Name"),
             start_time: TimeValue::UnsignedInt(1697462400000),
             end_time: Some(TimeValue::UnsignedInt(1697466000000)),
-            run_type: String::from("test_run_type"),
+            run_type: String::from("chain"),
             reference_example_id: None,
         },
         attachments,
+        io: RunIO {
+            inputs: serde_json::json!({"input": "value"}),
+            outputs: Some(serde_json::json!({"output": "value"})),
+        },
     };
 
     client.submit_run_create(run_create).await.unwrap();
@@ -146,13 +143,69 @@ async fn test_tracing_client_submit_run_create() {
     let fields = handle_request(req.0, req.1);
 
     assert_eq!(fields.len(), 5);
+
+    // assert run fields
     assert_eq!(fields[0].name, "post.test_id");
     assert_eq!(fields[0].content_type, Some("application/json".to_string()));
     assert_eq!(fields[0].filename, None);
+    let received_run: serde_json::Value = serde_json::from_str(&fields[0].data).unwrap();
+    assert_eq!(received_run["id"], "test_id");
+    assert_eq!(received_run["trace_id"], "trace_id");
+    assert_eq!(received_run["dotted_order"], "1.1");
+    assert_eq!(received_run["parent_run_id"], serde_json::Value::Null);
+    assert_eq!(
+        received_run["extra"],
+        serde_json::json!({"extra_data": "value"})
+    );
+    assert_eq!(received_run["error"], serde_json::Value::Null);
+    assert_eq!(
+        received_run["serialized"],
+        serde_json::json!({"key": "value"})
+    );
+    assert_eq!(
+        received_run["events"],
+        serde_json::json!([{ "event": "event_data" }])
+    );
+    assert_eq!(received_run["tags"], serde_json::json!([{"tag": "value"}]));
+    assert_eq!(received_run["session_name"], "Session Name");
+    assert_eq!(received_run["session_id"], serde_json::Value::Null);
+    assert_eq!(received_run["name"], "Run Name");
+    assert_eq!(received_run["start_time"], 1697462400000i64);
+    assert_eq!(received_run["end_time"], 1697466000000i64);
+    assert_eq!(received_run["run_type"], "chain");
+    assert_eq!(
+        received_run["reference_example_id"],
+        serde_json::Value::Null
+    );
 
-    let received_run: RunCreate = serde_json::from_str(&fields[0].data).unwrap();
-    //assert_eq!(received_run, run_create.run_create);
+    // assert inputs fields
+    assert_eq!(fields[1].name, "post.test_id.inputs");
+    assert_eq!(fields[1].content_type, Some("application/json".to_string()));
+    assert_eq!(fields[1].filename, None);
+    let received_inputs: serde_json::Value = serde_json::from_str(&fields[1].data).unwrap();
+    assert_eq!(received_inputs, serde_json::json!({"input": "value"}));
 
+    // assert outputs fields
+    assert_eq!(fields[2].name, "post.test_id.outputs");
+    assert_eq!(fields[2].content_type, Some("application/json".to_string()));
+    assert_eq!(fields[2].filename, None);
+    let received_outputs: serde_json::Value = serde_json::from_str(&fields[2].data).unwrap();
+    assert_eq!(received_outputs, serde_json::json!({"output": "value"}));
+
+    // assert attachment_1 fields
+    assert_eq!(fields[3].name, "post.test_id.attachments.attachment_1");
+    assert_eq!(
+        fields[3].content_type,
+        Some("application/octet-stream".to_string())
+    );
+    assert_eq!(fields[3].filename, Some("file1.txt".to_string()));
+    assert_eq!(fields[3].data, "\u{1}\u{2}\u{3}");
+
+    // assert attachment_2 fields
+    assert_eq!(fields[4].name, "post.test_id.attachments.attachment_2");
+    assert_eq!(fields[4].content_type, Some("text/plain".to_string()));
+    assert_eq!(fields[4].filename, Some("test_file_create.txt".to_string()));
+    assert_eq!(fields[4].data, "Test file content for create\n");
 }
 
 // #[tokio::test]
