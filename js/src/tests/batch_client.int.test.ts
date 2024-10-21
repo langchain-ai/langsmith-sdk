8673ed1a1652514c0e1349947f5eb81b4bb354be
jacoblee93 <jacoblee93@gmail.com>
2024-10-21 10:12:04 -0700

    Adds test for attachments

--- js/src/tests/batch_client.int.test.ts
+++ js/src/tests/batch_client.int.test.ts
@@ -1,6 +1,10 @@
+import { v4 as uuidv4 } from "uuid";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { Client } from "../client.js";
 import { RunTree, convertToDottedOrderFormat } from "../run_trees.js";
-import { v4 as uuidv4 } from "uuid";
 import {
   deleteProject,
   waitUntilProjectFound,
@@ -181,6 +185,58 @@ test.concurrent(
     expect(storedRun.trace_id).toEqual(runTree.trace_id);
     expect(storedRun.inputs).toEqual({ input: "foo1" });
     expect(storedRun.outputs).toEqual({ output: "foo2" });
+    await langchainClient.deleteProject({ projectName });
+  },
+  180_000
+);
+
+test.concurrent(
+  "Test persist run with attachment",
+  async () => {
+    const langchainClient = new Client({
+      autoBatchTracing: true,
+      callerOptions: { maxRetries: 2 },
+      timeout_ms: 30_000,
+    });
+    const projectName = "__test_create_attachment" + uuidv4().substring(0, 4);
+    await deleteProject(langchainClient, projectName);
+
+    const runId = uuidv4();
+    const dottedOrder = convertToDottedOrderFormat(
+      new Date().getTime() / 1000,
+      runId
+    );
+    const pathname = path.join(
+      path.dirname(fileURLToPath(import.meta.url)),
+      "test_data",
+      "parrot-icon.png"
+    );
+    await langchainClient.createRun({
+      id: runId,
+      project_name: projectName,
+      name: "test_run",
+      run_type: "llm",
+      inputs: { text: "hello world" },
+      trace_id: runId,
+      dotted_order: dottedOrder,
+      attachments: {
+        testimage: ["image/png", fs.readFileSync(pathname)],
+      },
+    });
+
+    await langchainClient.updateRun(runId, {
+      outputs: { output: ["Hi"] },
+      dotted_order: dottedOrder,
+      trace_id: runId,
+    });
+
+    await Promise.all([
+      waitUntilRunFound(langchainClient, runId, true),
+      waitUntilProjectFound(langchainClient, projectName),
+    ]);
+
+    const storedRun = await langchainClient.readRun(runId);
+    expect(storedRun.id).toEqual(runId);
     await langchainClient.deleteProject({ projectName });
   },
   180_000
