ce68109e8265d39f5725310ce23fdd7abc66b2ef
Brendan Maginnis <brendan.maginnis@gmail.com>
2024-10-30 16:40:13 +0000

    Fix orjson serialization of dict keys
    Apply `_simple_default` serialization on dict keys to resolve this exception
    
    ```python
    Traceback (most recent call last):
      File "/Users/brendan/.local/share/virtualenvs/core-bRkH1-KB/lib/python3.11/site-packages/langsmith/client.py", line 281, in _dumps_json_single
        return orjson.dumps(
               ^^^^^^^^^^^^^
    TypeError: Dict key must a type serializable with OPT_NON_STR_KEYS
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "/Users/brendan/.local/share/virtualenvs/core-bRkH1-KB/lib/python3.11/site-packages/langsmith/client.py", line 5802, in _tracing_thread_handle_batch
        client.multipart_ingest_runs(create=create, update=update, pre_sampled=True)
      File "/Users/brendan/.local/share/virtualenvs/core-bRkH1-KB/lib/python3.11/site-packages/langsmith/client.py", line 1675, in multipart_ingest_runs
        valb = _dumps_json(value)
               ^^^^^^^^^^^^^^^^^^
      File "/Users/brendan/.local/share/virtualenvs/core-bRkH1-KB/lib/python3.11/site-packages/langsmith/client.py", line 321, in _dumps_json
        return _dumps_json_single(obj, _serialize_json)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/Users/brendan/.local/share/virtualenvs/core-bRkH1-KB/lib/python3.11/site-packages/langsmith/client.py", line 292, in _dumps_json_single
        result = json.dumps(
                 ^^^^^^^^^^^
      File "/usr/local/Cellar/python@3.11/3.11.10/Frameworks/Python.framework/Versions/3.11/lib/python3.11/json/__init__.py", line 238, in dumps
        **kw).encode(obj)
              ^^^^^^^^^^^
      File "/usr/local/Cellar/python@3.11/3.11.10/Frameworks/Python.framework/Versions/3.11/lib/python3.11/json/encoder.py", line 200, in encode
        chunks = self.iterencode(o, _one_shot=True)
                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/usr/local/Cellar/python@3.11/3.11.10/Frameworks/Python.framework/Versions/3.11/lib/python3.11/json/encoder.py", line 258, in iterencode
        return _iterencode(o, 0)
               ^^^^^^^^^^^^^^^^^
    TypeError: keys must be str, int, float, bool or None, not PosixPath
    ```

--- python/langsmith/_internal/_serde.py
+++ python/langsmith/_internal/_serde.py
@@ -37,6 +37,8 @@ def _simple_default(obj):
             return obj.isoformat()
         if isinstance(obj, uuid.UUID):
             return str(obj)
+        if isinstance(obj, dict):
+            return {_simple_default(key): value for key, value in obj.items()}
         if hasattr(obj, "model_dump") and callable(obj.model_dump):
             return obj.model_dump()
         elif hasattr(obj, "dict") and callable(obj.dict):
