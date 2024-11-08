df2c0a5a4d9357079bc922629055e293696e8137
isaac hershenson <ihershenson@hmc.edu>
2024-11-08 11:57:46 -0800

    debugging

--- js/src/tests/llm_evaluator.int.test.ts
+++ js/src/tests/llm_evaluator.int.test.ts
@@ -148,7 +148,7 @@ test("llm evaluator can evaluate runs", async () => {
   const firstResult = evalRes.results[0];
 
   const evaluation = firstResult.evaluationResults.results[0];
-  console.log("FIRST", firstResult)
+  console.log("FIRST", firstResult.evaluationResults.results)
   expect(evaluation.key).toBe("vagueness");
   expect(["Y", "N"]).toContain(evaluation.value);
   expect(evaluation.comment).toBeDefined();
