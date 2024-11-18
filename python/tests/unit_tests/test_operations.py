bdd9f0a4ed5e92f9fd2f0c4497b3f09500b2f7f9
Erick Friis <erick@langchain.dev>
2024-11-18 13:19:01 -0800

    x

--- python/tests/unit_tests/test_operations.py
+++ python/tests/unit_tests/test_operations.py
@@ -20,7 +20,6 @@ def test_combine_serialized_queue_operations():
             attachments=None,
         ),
         SerializedRunOperation(
-            operation="patch",
             id="id1",
             trace_id="trace_id1",
             _none=_orjson.dumps({"b": "2"}),
