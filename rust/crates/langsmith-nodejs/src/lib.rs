7692d419d9f21c329501f830ea95debaea59741c
Predrag Gruevski <obi1kenobi82@gmail.com>
2024-12-05 21:00:32 +0000

    Reformat Rust files.

--- rust/crates/langsmith-nodejs/src/lib.rs
+++ rust/crates/langsmith-nodejs/src/lib.rs
@@ -4,8 +4,8 @@ use napi_derive::napi;
 
 #[napi]
 pub fn fibonacci(n: u32) -> u32 {
-  match n {
-    1 | 2 => 1,
-    _ => fibonacci(n - 1) + fibonacci(n - 2),
-  }
+    match n {
+        1 | 2 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
 }
