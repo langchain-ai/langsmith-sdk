8a4530488e759003994c06ddbe11b82737f1e7e0
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2024-12-09 16:29:29 -0500

    Update js/src/client.ts

--- js/src/client.ts
+++ js/src/client.ts
@@ -487,7 +487,6 @@ export class Client implements LangSmithTracingClientInterface {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private _getServerInfoPromise?: Promise<Record<string, any>>;
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private _rustClient: bindings.TracingClient | null;
 
   constructor(config: ClientConfig = {}) {
