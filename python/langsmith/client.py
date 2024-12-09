1abe4f9a6ffd805fc902540df5e60cb7f6449c58
isaac hershenson <ihershenson@hmc.edu>
2024-12-09 13:02:26 -0800

    flag

--- python/langsmith/client.py
+++ python/langsmith/client.py
@@ -3598,7 +3598,7 @@ class Client:
     ) -> ls_schemas.UpsertExamplesResponse:
         """Upload examples."""
         if not (self.info.instance_flags or {}).get(
-            "examples_multipart_enabled", False
+            "dataset_examples_multipart_enabled", False
         ):
             raise ValueError(
                 "Your LangSmith version does not allow using the multipart examples endpoint, please update to the latest version."
