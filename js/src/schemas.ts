a607b4e00d69157a91bd087a3ea53c69574350d2
isaac hershenson <ihershenson@hmc.edu>
2024-12-11 12:29:48 -0800

    x

--- js/src/schemas.ts
+++ js/src/schemas.ts
@@ -67,7 +67,7 @@ export interface AttachmentInfo {
   presigned_url: string;
 }
 
-export type AttachmentData = Uint8Array | Blob;
+export type AttachmentData = ArrayBuffer | Uint8Array | Blob;
 export type Attachments = Record<string, [string, AttachmentData]>;
 
 /**
