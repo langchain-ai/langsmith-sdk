f9aac67d39fb7f34691f975d24246b3433d18d87
Angus Jelinek <angus@langchain.dev>
2024-12-19 18:23:34 -0800

    Make zstandard level configurable

--- python/langsmith/_internal/_compressed_runs.py
+++ python/langsmith/_internal/_compressed_runs.py
@@ -8,6 +8,10 @@ try:
 except ImportError:
     HAVE_ZSTD = False
 
+from langsmith import utils as ls_utils
+
+compression_level = ls_utils.get_env_var("RUN_COMPRESSION_LEVEL", 3)
+
 
 class CompressedRuns:
     def __init__(self):
@@ -19,9 +23,9 @@ class CompressedRuns:
                 "zstandard package required for compression. "
                 "Install with 'pip install langsmith[compression]'"
             )
-        self.compressor_writer = ZstdCompressor(level=3, threads=-1).stream_writer(
-            self.buffer, closefd=False
-        )
+        self.compressor_writer = ZstdCompressor(
+            level=compression_level, threads=-1
+        ).stream_writer(self.buffer, closefd=False)
 
     def reset(self):
         self.buffer = io.BytesIO()
@@ -31,6 +35,6 @@ class CompressedRuns:
                 "zstandard package required for compression. "
                 "Install with 'pip install langsmith[compression]'"
             )
-        self.compressor_writer = ZstdCompressor(level=3, threads=-1).stream_writer(
-            self.buffer, closefd=False
-        )
+        self.compressor_writer = ZstdCompressor(
+            level=compression_level, threads=-1
+        ).stream_writer(self.buffer, closefd=False)
