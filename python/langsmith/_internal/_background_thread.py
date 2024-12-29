512da14f854b8dbfaf1155f95193ae5a6063f142
Angus Jelinek <angus@langchain.dev>
2024-12-29 16:37:33 -0500

    feat: Make size limit (run count) globally configurable via env var

--- python/langsmith/_internal/_background_thread.py
+++ python/langsmith/_internal/_background_thread.py
@@ -18,6 +18,8 @@ from typing import (
 )
 
 from langsmith import schemas as ls_schemas
+from langsmith import utils as ls_utils
+
 from langsmith._internal._constants import (
     _AUTO_SCALE_DOWN_NEMPTY_TRIGGER,
     _AUTO_SCALE_UP_NTHREADS_LIMIT,
@@ -155,6 +157,16 @@ def _tracing_thread_handle_batch(
         for _ in batch:
             tracing_queue.task_done()
 
+def get_size_limit_from_env() -> Optional[int]:
+    size_limit_str = ls_utils.get_env_var(
+        "BATCH_INGEST_SIZE_LIMIT",
+    )
+    if size_limit_str is not None:
+        try:
+            return int(size_limit_str)
+        except ValueError:
+            logger.warning("Invalid value for BATCH_INGEST_SIZE_LIMIT: %s, continuing with default", size_limit_str)
+    return None
 
 def _ensure_ingest_config(
     info: ls_schemas.LangSmithInfo,
@@ -172,6 +184,9 @@ def _ensure_ingest_config(
     try:
         if not info.batch_ingest_config:
             return default_config
+        env_size_limit = get_size_limit_from_env()
+        if env_size_limit is not None:
+            info.batch_ingest_config["size_limit"] = env_size_limit
         return info.batch_ingest_config
     except BaseException:
         return default_config
