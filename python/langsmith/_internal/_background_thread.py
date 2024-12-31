abb342d4f6ad515e2f82630745906bb254f64eb6
angus-langchain <angus@langchain.dev>
2024-12-30 21:45:35 -0500

    fix: Flush zstd and correctly get buffer size (#1353)
    ### Purpose 
    Correctly determine the size of compressed runs buffer before checking
    if we should flush it.
    
    ### Changes
    Periodically flush zstd buffer and get the correct size.

--- python/langsmith/_internal/_background_thread.py
+++ python/langsmith/_internal/_background_thread.py
@@ -100,6 +100,7 @@ def _tracing_thread_drain_compressed_buffer(
 ) -> Optional[io.BytesIO]:
     assert client.compressed_runs is not None
     with client.compressed_runs.lock:
+        client.compressed_runs.compressor_writer.flush()
         current_size = client.compressed_runs.buffer.tell()
 
         if size_limit is not None and size_limit <= 0:
