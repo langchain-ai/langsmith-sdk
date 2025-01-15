68b47ff64d72a410493959396ec295397c2e522a
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-01-15 19:04:06 +0000

    Re-apply patch needed to make orjson run.

--- vendor/pyo3/pyo3-ffi/src/object.rs
+++ vendor/pyo3/pyo3-ffi/src/object.rs
@@ -214,7 +214,7 @@ pub unsafe fn Py_SIZE(ob: *mut PyObject) -> Py_ssize_t {
 
 #[inline(always)]
 #[cfg(all(Py_3_12, not(Py_GIL_DISABLED)))]
-unsafe fn _Py_IsImmortal(op: *mut PyObject) -> c_int {
+pub unsafe fn _Py_IsImmortal(op: *mut PyObject) -> c_int {
     #[cfg(target_pointer_width = "64")]
     {
         (((*op).ob_refcnt.ob_refcnt as crate::PY_INT32_T) < 0) as c_int
