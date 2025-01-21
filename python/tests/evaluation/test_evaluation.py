0557206ecdc21a7eb5bef2c9e7bf339a4a44ffe8
Bagatur <baskaryan@gmail.com>
2025-01-21 14:55:14 -0800

    concurrency

--- python/tests/evaluation/test_evaluation.py
+++ python/tests/evaluation/test_evaluation.py
@@ -234,6 +234,7 @@ def test_evaluate():
             "function": "evaluate",
         },
         num_repetitions=3,
+        max_concurrency=2,
     )
     assert len(results) == 30
     if _has_pandas():
@@ -327,6 +328,7 @@ async def test_aevaluate():
             "function": "aevaluate",
         },
         num_repetitions=2,
+        max_concurrency=2,
     )
     assert len(results) == 20
     if _has_pandas():
