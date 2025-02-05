84f8d391e54e82e02474f4137af2890ce3b388d3
Angus Jelinek <angus@langchain.dev>
2025-02-04 17:22:31 -0800

    lint python

--- python/langsmith/evaluation/_arunner.py
+++ python/langsmith/evaluation/_arunner.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 import asyncio
 import concurrent.futures as cf
-import datetime
 import io
 import logging
 import pathlib
--- python/langsmith/evaluation/_runner.py
+++ python/langsmith/evaluation/_runner.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 import ast
 import collections
 import concurrent.futures as cf
-import datetime
 import functools
 import inspect
 import io
