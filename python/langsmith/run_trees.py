4c704340333e130d9f2e196768fd37238748fce0
Nuno Campos <nuno@boringbits.io>
2025-06-10 14:23:32 -0700

    Remove outdated _LOCK comment

--- python/langsmith/run_trees.py
+++ python/langsmith/run_trees.py
@@ -37,7 +37,7 @@ LANGSMITH_PROJECT = sys.intern(f"{LANGSMITH_PREFIX}project")
 OVERRIDE_OUTPUTS = sys.intern("__omit_auto_outputs")
 NOT_PROVIDED = cast(None, object())
 _CLIENT: Optional[Client] = None
-_LOCK = threading.Lock()  # Keeping around for a while for backwards compat
+_LOCK = threading.Lock()
 
 # Context variables
 _REPLICAS = contextvars.ContextVar[Optional[Sequence[tuple[str, Optional[dict]]]]](
@@ -51,8 +51,9 @@ _REPLICAS = contextvars.ContextVar[Optional[Sequence[tuple[str, Optional[dict]]]
 def get_cached_client(**init_kwargs: Any) -> Client:
     global _CLIENT
     if _CLIENT is None:
-        if _CLIENT is None:
-            _CLIENT = Client(**init_kwargs)
+        with _LOCK:
+            if _CLIENT is None:
+                _CLIENT = Client(**init_kwargs)
     return _CLIENT
 
 
