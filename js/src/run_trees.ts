e5589f3e434eb4f426182370ec22849c67756611
jacoblee93 <jacoblee93@gmail.com>
2025-06-13 16:25:05 -0700

    Avoid overwriting runtime if already set

--- js/src/run_trees.ts
+++ js/src/run_trees.ts
@@ -368,18 +368,21 @@ export class RunTree implements BaseRun {
     runtimeEnv: RuntimeEnvironment | undefined,
     excludeChildRuns = true
   ): RunCreate {
-    const runExtra = run.extra ?? {};
-    if (!runExtra.runtime) {
-      runExtra.runtime = {};
-    }
-    if (runtimeEnv) {
-      for (const [k, v] of Object.entries(runtimeEnv)) {
-        if (!runExtra.runtime[k]) {
-          runExtra.runtime[k] = v;
+    // Avoid overwriting the runtime environment if it's already set
+    if (run.extra?.runtime?.library === undefined) {
+      const runExtra = run.extra ?? {};
+      if (!runExtra.runtime) {
+        runExtra.runtime = {};
+      }
+      if (runtimeEnv) {
+        for (const [k, v] of Object.entries(runtimeEnv)) {
+          if (!runExtra.runtime[k]) {
+            runExtra.runtime[k] = v;
+          }
         }
       }
     }
-
+ 
     let child_runs: RunCreate[];
     let parent_run_id: string | undefined;
     if (!excludeChildRuns) {
