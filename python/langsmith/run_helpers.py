c1579ee8893ee63df97fe67ccfc26034361c5e15
Angus Jelinek <angus@langchain.dev>
2025-06-17 14:53:45 -0700

    fix

--- python/langsmith/run_helpers.py
+++ python/langsmith/run_helpers.py
@@ -1993,10 +1993,9 @@ def _maybe_create_otel_context(run_tree: Optional[run_trees.RunTree]):
     Returns:
         Context manager for use_span or None if not available.
     """
-    if not run_tree or not utils.is_truish(utils.get_env_var("OTEL_ENABLED")):
-        return None
-
     if not _is_otel_available():
+        return None
+    if not run_tree or not utils.is_truish(utils.get_env_var("OTEL_ENABLED")):
         return None
 
     from opentelemetry.trace import (  # type: ignore[import]
