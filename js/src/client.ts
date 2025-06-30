c20d305b78704229f614aa72ccbe595fb0386a70
jacoblee93 <jacoblee93@gmail.com>
2025-06-30 09:21:23 -0700

    Add extra check for OTEL

--- js/src/client.ts
+++ js/src/client.ts
@@ -5140,7 +5140,9 @@ export class Client implements LangSmithTracingClientInterface {
       ...this.autoBatchQueue.items.map(({ itemPromise }) => itemPromise),
       this.batchIngestCaller.queue.onIdle(),
     ]);
-    await getDefaultOTLPTracerComponents()?.DEFAULT_LANGSMITH_SPAN_PROCESSOR?.forceFlush();
+    if (getEnvironmentVariable("OTEL_ENABLED") === "true") {
+      await getDefaultOTLPTracerComponents()?.DEFAULT_LANGSMITH_SPAN_PROCESSOR?.forceFlush();
+    }
   }
 }
 
