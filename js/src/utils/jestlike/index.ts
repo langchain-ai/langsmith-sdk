eb31cb1b50d8b7554cd12556896c6bf2fddf47cb
Liam <liam@ten.dev>
2025-07-22 13:12:31 +0100

    fix(js): Rename iteration parameter to repetition in generateWrapperFromJestlikeMethods

--- js/src/utils/jestlike/index.ts
+++ js/src/utils/jestlike/index.ts
@@ -553,7 +553,7 @@ export function generateWrapperFromJestlikeMethods(
                           ...rest,
                           inputs: testInput,
                           referenceOutputs: testOutput,
-                          iteration: i,
+                          repetition: i,
                         }
                       )
                     );
