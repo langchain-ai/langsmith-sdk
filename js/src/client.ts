69f8726710af9160ce43f9e5dfc0e20d7dcb9345
angus-langchain <angus@langchain.dev>
2025-07-31 11:32:46 -0700

    patch(python, js): Fix sampling logic for patch runs (#1881)
    ### Description
    We were not correctly filtering patch runs when the post would get
    filtered, meaning large amounts of patch requests were being sent to
    ingest API and counting toward usage limits, but not actually getting
    ingested.
    
    ### Changes
    Check patch trace id against trace id list, only remove trace ids from
    list for root runs.
    
    ### Alternative Solutions
    Use consistent hashing based on run_id to sample both post and patch run
    ids equally, prevents storage of uuids (minimal memory overhead)
    
    ## Tests
    Added unit tests confirming filtering logic correctly applies to both
    post and patch

--- js/src/client.ts
+++ js/src/client.ts
@@ -935,10 +935,10 @@ export class Client implements LangSmithTracingClientInterface {
     if (patch) {
       const sampled = [];
       for (const run of runs) {
-        if (!this.filteredPostUuids.has(run.id)) {
+        if (!this.filteredPostUuids.has(run.trace_id)) {
           sampled.push(run);
-        } else {
-          this.filteredPostUuids.delete(run.id);
+        } else if (run.id === run.trace_id) {
+          this.filteredPostUuids.delete(run.trace_id);
         }
       }
       return sampled;
