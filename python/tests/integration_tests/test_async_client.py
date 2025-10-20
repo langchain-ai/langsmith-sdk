35326a0677ea0597bd74c56a8b1a8b408753be2f
jacoblee93 <jacoblee93@gmail.com>
2025-10-20 14:33:06 -0700

    Bump timeout

--- python/tests/integration_tests/test_async_client.py
+++ python/tests/integration_tests/test_async_client.py
@@ -148,7 +148,7 @@ async def test_list_runs(async_client: AsyncClient):
         ]
         return len(runs) == 3
 
-    await wait_for(check_runs, timeout=20)
+    await wait_for(check_runs, timeout=30)
 
     runs = [
         run
