6f319aa4c0011252bbde45a38629ba485e4d5961
Angus Jelinek <angus@langchain.dev>
2025-11-20 11:20:48 -0800

    merge

--- python/langsmith/__init__.py
+++ python/langsmith/__init__.py
@@ -22,7 +22,7 @@ if TYPE_CHECKING:
 
 # Avoid calling into importlib on every call to __version__
 
-__version__ = "0.4.43"
+__version__ = "0.4.44"
 version = __version__  # for backwards compatibility
 
 
