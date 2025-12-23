dd358702dca80cc50c1a995583ab85e44b85d287
Eugene Yurtsev <eyurtsev@gmail.com>
2025-12-23 17:02:21 -0500

    x

--- python/langsmith/__init__.py
+++ python/langsmith/__init__.py
@@ -28,7 +28,7 @@ if TYPE_CHECKING:
 
 # Avoid calling into importlib on every call to __version__
 
-__version__ = "0.5.0"
+__version__ = "0.5.1a1"
 version = __version__  # for backwards compatibility
 
 
