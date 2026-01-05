6a2d9935312ddc301bfbb7f78e9828cf3261b935
langchain-infra <mukil@langchain.dev>
2026-01-05 13:19:13 -0500

    ci: test more bundlers (#2263)
    Validate our SDK builds with some more bundlers

---
+++ js/internal/environment_tests/test-exports-metro/src/index.js
@@ -0,0 +1,20 @@
+/**
+ * Metro bundler test for langsmith exports.
+ * Tests that public API works correctly when bundled with Metro.
+ *
+ * This test verifies:
+ * 1. Metro can bundle langsmith
+ * 2. The browser field in package.json correctly swaps Node-specific code
+ * 3. Client export is available
+ */
+
+import { Client } from "langsmith";
+
+// Export to prevent tree-shaking
+export { Client };
+
+// Simple test that instantiation works
+export function testExports() {
+  const client = new Client({ apiKey: "test-key" });
+  return { client };
+}
