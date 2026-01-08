8725abc9c76fe92c0ab16ce9662d8fea167a805d
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2026-01-08 14:06:07 -0800

    fixup test

--- python/langsmith/async_client.py
+++ python/langsmith/async_client.py
@@ -1526,17 +1526,21 @@ class AsyncClient:
                 "Prompt does not exist, you must create it first."
             )
 
-        try:
-            from langchain_core.load import dumps
-        except ImportError:
-            raise ImportError(
-                "The client.create_commit function requires the langchain-core"
-                "package to run.\nInstall with `pip install langchain-core`"
-            )
+        # Check if object is already a serialized LangChain manifest
+        prepped = ls_client.prep_obj_for_push(object)
+        if isinstance(prepped, dict) and "id" in prepped and "lc" in prepped:
+            manifest_dict = prepped
+        else:
+            try:
+                from langchain_core.load import dumps
+            except ImportError:
+                raise ImportError(
+                    "The client.create_commit function requires the langchain-core"
+                    "package to run.\nInstall with `pip install langchain-core`"
+                )
 
-        chain_to_push = ls_client.prep_obj_for_push(object)
-        json_object = dumps(chain_to_push)
-        manifest_dict = json.loads(json_object)
+            json_object = dumps(prepped)
+            manifest_dict = json.loads(json_object)
 
         owner, prompt_name, _ = ls_utils.parse_prompt_identifier(prompt_identifier)
         prompt_owner_and_name = f"{owner}/{prompt_name}"
