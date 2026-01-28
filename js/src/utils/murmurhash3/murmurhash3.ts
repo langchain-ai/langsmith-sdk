84cf80c67de26cb443f2100449217279fe15524b
jacoblee93 <jacoblee93@gmail.com>
2026-01-27 20:53:33 -0800

    Fix

--- js/src/utils/murmurhash3/murmurhash3.ts
+++ js/src/utils/murmurhash3/murmurhash3.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 // murmurhash3 - a fast non-cryptographic hash function
 // https://caolan.uk/src/murmurhash3/about
 //
