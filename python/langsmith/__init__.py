09694c3e8fd143d03690e42dcf5d911469db4744
Angus Jelinek <angus@langchain.dev>
2026-02-13 15:21:26 -0800

    Bump version: 0.7.2 → 0.7.3

--- python/langsmith/__init__.py
+++ python/langsmith/__init__.py
@@ -29,7 +29,7 @@ if TYPE_CHECKING:
 
 # Avoid calling into importlib on every call to __version__
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 version = __version__  # for backwards compatibility
 
 
