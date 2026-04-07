96e330ce30178b45084f329fc90c1f4026897fda
Jacob Lee <jacoblee93@gmail.com>
2026-04-07 09:31:55 -0700

    chore(js): Remove Chalk dep (#2664)

---
+++ js/src/utils/console-table-printer/src/models/common.ts
@@ -0,0 +1,17 @@
+import { ALIGNMENTS, COLORS } from '../utils/table-constants.js';
+
+export type ALIGNMENT = (typeof ALIGNMENTS)[number];
+
+export type COLOR = (typeof COLORS)[number];
+export interface Dictionary {
+  [key: string]: any;
+}
+
+export interface CharLengthDict {
+  [key: string]: number;
+}
+export interface Row {
+  color: COLOR;
+  separator: boolean;
+  text: Dictionary;
+}
