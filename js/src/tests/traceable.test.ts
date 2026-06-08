b4e06ef68ce0c71025038dd1810245fb0047035c
John Kennedy <65985482+jkennedyvz@users.noreply.github.com>
2026-06-07 19:09:43 -0700

    test: use supported Jest throw matcher

--- js/src/tests/traceable.test.ts
+++ js/src/tests/traceable.test.ts
@@ -2430,7 +2430,7 @@ test("traceable wrapper with getCurrentRunTree", async () => {
   const { client } = mockClient();
 
   // Called outside a traceable function - should throw
-  expect(() => getCurrentRunTree()).toThrowError();
+  expect(() => getCurrentRunTree()).toThrow();
 
   const runId = uuidv4();
   const nestedAddValueTraceable = traceable(
