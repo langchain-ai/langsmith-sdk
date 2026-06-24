c7db0d1de8f7ee39c86a7bb206d7ac29d8a7dd91
Kiewan Villatel <kiewan@langchain.dev>
2026-06-24 11:50:49 +0200

    Add deprecation warning on JS methods

--- js/src/client.ts
+++ js/src/client.ts
@@ -2743,6 +2743,7 @@ export class Client implements LangSmithTracingClientInterface {
     });
   }
 
+  /** @deprecated Use `client.runs.retrieve(...)` instead. */
   public async readRun(
     runId: string,
     { loadChildRuns }: { loadChildRuns: boolean } = { loadChildRuns: false },
@@ -2838,6 +2839,8 @@ export class Client implements LangSmithTracingClientInterface {
   }
 
   /**
+   * @deprecated Use `client.runs.query(...)` instead.
+   *
    * List runs from the LangSmith server.
    * @param projectId - The ID of the project to filter by.
    * @param projectName - The name of the project to filter by.
