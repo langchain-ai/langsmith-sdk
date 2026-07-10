53d34e1508ed7ba03ced9db2fce6e1582b4e4c35
Kiewan Villatel <kiewan@langchain.dev>
2026-07-10 14:29:07 +0200

    release(py): 0.10.2

--- python/langsmith/__init__.py
+++ python/langsmith/__init__.py
@@ -45,7 +45,7 @@ if TYPE_CHECKING:
 
 # Avoid calling into importlib on every call to __version__
 
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 version = __version__  # for backwards compatibility
 
 # Metadata key to hide a traced run from LangSmith's Messages View.
